77a6ced89e8dea6dd127bcacbc6192bdc053c882
Jaime <jaimeloeuf@gmail.com>
2024-07-08 00:02:14 +0800

    Change JSend default generic type.

--- api/src/http/JSend.ts
+++ api/src/http/JSend.ts
@@ -6,9 +6,20 @@
  */
 
 /**
+ * Default JSend usable data type is mostly just any datatype that is JSON
+ * serializable.
+ */
+export type DefaultJsendDatatype =
+  | null
+  | string
+  | number
+  | Array<unknown>
+  | Record<string, unknown>;
+
+/**
  * On successful execution of an action.
  */
-export type JSendSuccess<T = unknown> = {
+export type JSendSuccess<T = DefaultJsendDatatype> = {
   status: "success";
   data: T;
 };
@@ -18,7 +29,7 @@ export type JSendSuccess<T = unknown> = {
  * recoverable failure modes, e.g. user tried to delete a piece of data that is
  * already deleted.
  */
-export type JSendFail<T = unknown> = {
+export type JSendFail<T = DefaultJsendDatatype> = {
   status: "fail";
   data: T;
 };
@@ -27,7 +38,7 @@ export type JSendFail<T = unknown> = {
  * On action execution error. Used to represent errors, i.e. something that went
  * wrong when it should not have happened, e.g. DB connection error.
  */
-export type JSendError<T = unknown> = {
+export type JSendError<T = DefaultJsendDatatype> = {
   status: "error";
   message: string;
   code?: number;
