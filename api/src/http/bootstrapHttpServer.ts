700b9c40696a69c7e62c0334c64255240bf9a7ee
Jaime <jaimeloeuf@gmail.com>
2024-07-09 00:07:34 +0800

    Reimplement not found route handler with exception.

--- api/src/http/bootstrapHttpServer.ts
+++ api/src/http/bootstrapHttpServer.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import { config } from "../config/index.js";
 import { logger } from "../logging/index.js";
 
+import { routeNotFound } from "./routeNotFound.js";
+
 /**
  * Bootstraps a web server using ExpressJS to route incoming HTTP requests to
  * HTTP controllers.
@@ -23,17 +25,7 @@ export function bootstrapHttpServer() {
 
     // Since this is the last non-error-handling route handler used, assume 404
     // as no other route handler responded.
-    .use((req, res) => {
-      res.status(404);
-
-      if (req.accepts("json")) {
-        res.json({ error: "Not found" });
-        return;
-      }
-
-      // Defaults to plain-text
-      res.type("txt").send("Not found");
-    });
+    .use(routeNotFound);
 
   app.listen(config.port, () => {
     logger.info(
---
+++ api/src/http/routeNotFound.ts
@@ -0,0 +1,6 @@
+import { expressWrapper } from "./expressWrapper.js";
+import { NotFoundException } from "../exceptions/index.js";
+
+export const routeNotFound = expressWrapper(() => {
+  throw new NotFoundException("API Route not found");
+});
