46c20d5223771eb007c5ddd81550d89c96a3c5f6
Jaime <jaimeloeuf@gmail.com>
2024-07-14 00:27:49 +0800

    Refactor out the Logger class.

--- api/src/logging/index.ts
+++ api/src/logging/index.ts
@@ -1,29 +1,5 @@
-import { colorizeByLogLevel } from "./colorizeByLogLevel.js";
-import type { LogLevel } from "./LogLevel.js";
-
 /**
- * Super simple custom logger, might change to use pino/winston in the future.
+ * Barrel file
  */
-class Logger {
-  log(level: LogLevel, label: string, ...args: Parameters<typeof console.log>) {
-    const formattedLogOutput =
-      `${new Date().toISOString()} ${level}: [${label}] ` +
-      args.flat().join(" ");
 
-    console.log(colorizeByLogLevel(level, formattedLogOutput));
-  }
-
-  error(label: string, ...args: Parameters<typeof console.error>) {
-    this.log("Error", label, args);
-  }
-
-  info(label: string, ...args: Parameters<typeof console.log>) {
-    this.log("Info", label, args);
-  }
-
-  verbose(label: string, ...args: Parameters<typeof console.log>) {
-    this.log("Verbose", label, args);
-  }
-}
-
-export const logger = new Logger();
+export * from "./Logger.js";
