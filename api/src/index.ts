81370dfe5aa2e879382e6267623951ef06cc9e6d
Jaime <jaimeloeuf@gmail.com>
2024-07-15 21:44:04 +0800

    Wrap main entrypoint to bootstrap sequentially.

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,13 @@
 import { registerGlobalUncaughtIssueHandlers } from "./registerGlobalUncaughtIssueHandlers.js";
 import { bootstrapHttpServer } from "./http/index.js";
 
-registerGlobalUncaughtIssueHandlers();
-bootstrapHttpServer();
+/**
+ * All bootstrapping calls wrapped in async main function to ensure they run
+ * sequentially and only run the next one after each is complete.
+ */
+async function main() {
+  registerGlobalUncaughtIssueHandlers();
+  bootstrapHttpServer();
+}
+
+main();
