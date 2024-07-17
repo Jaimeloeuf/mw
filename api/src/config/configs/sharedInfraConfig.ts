8644c355b0d89db17d2d487221b571499def30a8
Jaime <jaimeloeuf@gmail.com>
2024-07-17 22:12:54 +0800

    Add DB connection string to config.

--- api/src/config/configs/sharedInfraConfig.ts
+++ api/src/config/configs/sharedInfraConfig.ts
@@ -2,6 +2,11 @@ import { z, type ZodRawShape } from "zod";
 
 export const sharedInfraConfigSchema = {
   /**
+   * Database Connection String
+   */
+  db_conn_string: z.string(),
+
+  /**
    * Telegram bot's token.
    * Placeholder can be used for development environments as it will be mocked.
    */
@@ -15,6 +20,7 @@ export const sharedInfraConfigSchema = {
 } satisfies ZodRawShape;
 
 export const sharedInfraConfigData = {
+  db_conn_string: process.env["DB_CONN_STRING"],
   tele_adminbot_token: process.env["TELE_ADMINBOT_TOKEN"],
   tele_adminbot_admin_chat_id: process.env["TELE_ADMINBOT_ADMIN_CHAT_ID"],
 } satisfies Record<keyof typeof sharedInfraConfigSchema, unknown>;
