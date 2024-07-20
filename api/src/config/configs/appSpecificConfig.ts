6ad2ca5bcf4fbc77fce2ba3d20615c97691bcc88
Jaime <jaimeloeuf@gmail.com>
2024-07-20 15:40:32 +0800

    Update blog email config:     To remove "transactional" since the email should be used for both     transactional emails and newsletter blasts.

--- api/src/config/configs/appSpecificConfig.ts
+++ api/src/config/configs/appSpecificConfig.ts
@@ -12,23 +12,16 @@ export const appSpecificConfigSchema = {
   /**
    * Email address used to send out transactional emails.
    */
-  blog_email_transactional_address: z
-    .string()
-    .email()
-    .default("robot@jjss.quest"),
+  blog_email_address: z.string().email().default("robot@jjss.quest"),
 
   /**
    * The 'reply to email address' users see in their transactional emails.
    */
-  blog_email_transactional_reply: z
-    .string()
-    .email()
-    .default("jaimeloeuf@gmail.com"),
+  blog_email_reply: z.string().email().default("jaimeloeuf@gmail.com"),
 } satisfies ZodRawShape;
 
 export const appSpecificConfigData = {
   blog_recaptcha_secret: process.env["BLOG_RECAPTCHA_SECRET"],
-  blog_email_transactional_address:
-    process.env["BLOG_EMAIL_TRANSACTIONAL_ADDRESS"],
-  blog_email_transactional_reply: process.env["BLOG_EMAIL_TRANSACTIONAL_REPLY"],
+  blog_email_address: process.env["BLOG_EMAIL_ADDRESS"],
+  blog_email_reply: process.env["BLOG_EMAIL_REPLY"],
 } satisfies Record<keyof typeof appSpecificConfigSchema, unknown>;
