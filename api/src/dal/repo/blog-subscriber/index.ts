f7d0bdfb77da15f6ad6553864b0fe5e156f1107d
Jaime <jaimeloeuf@gmail.com>
2024-07-21 01:16:16 +0800

    Update blogSubscriberRepo to use dalNoThrow.

--- api/src/dal/repo/blog-subscriber/index.ts
+++ api/src/dal/repo/blog-subscriber/index.ts
@@ -1,11 +1,13 @@
+import { dalNoThrow } from "../dalNoThrow.js";
+
 import { getAllBlogSubscriber } from "./getAllBlogSubscriber.js";
 import { createBlogSubscriber } from "./createBlogSubscriber.js";
 import { deleteBlogSubscriber } from "./deleteBlogSubscriber.js";
 import { isEmailAlreadySubscribed } from "./isEmailAlreadySubscribed.js";
 
 export const blogSubscriberRepo = {
-  getAllBlogSubscriber,
-  createBlogSubscriber,
-  deleteBlogSubscriber,
-  isEmailAlreadySubscribed,
+  getAllBlogSubscriber: dalNoThrow(getAllBlogSubscriber),
+  createBlogSubscriber: dalNoThrow(createBlogSubscriber),
+  deleteBlogSubscriber: dalNoThrow(deleteBlogSubscriber),
+  isEmailAlreadySubscribed: dalNoThrow(isEmailAlreadySubscribed),
 };
