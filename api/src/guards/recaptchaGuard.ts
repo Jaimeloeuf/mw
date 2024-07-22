7257365467d1f1ef2f8550815dd669147a35147c
Jaime <jaimeloeuf@gmail.com>
2024-07-22 23:22:55 +0800

    Add mockRecaptchaGuard to use when not in prod.

--- api/src/guards/recaptchaGuard.ts
+++ api/src/guards/recaptchaGuard.ts
@@ -1,5 +1,6 @@
 import { sf } from "simpler-fetch";
 import type { Request } from "express";
+import { config } from "../config/index.js";
 import { ForbiddenException } from "../exceptions/index.js";
 import type { HttpRequestGuard } from "../http/HttpRequestGuard.js";
 import { prettyPrintJson } from "../utils/index.js";
@@ -20,44 +21,49 @@ export const recaptchaGuardFF = (
    */
   scoreRequirement: number = 0.5
 ) =>
-  async function recaptchaGuard(
-    /**
-     * The underlying Express request object
-     */
-    req: Request
-  ) {
-    // Get the remote IP address of the user, since this service will be ran
-    // behind a proxy, `x-forwarded-for` is used first to prevent using the
-    // proxy's IP address as the value since it is always the same.
-    const remoteIP = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  config.env !== "production"
+    ? function mockRecaptchaGuard() {}
+    : (async function recaptchaGuard(
+        /**
+         * The underlying Express request object
+         */
+        req: Request
+      ) {
+        // Get the remote IP address of the user, since this service will be ran
+        // behind a proxy, `x-forwarded-for` is used first to prevent using the
+        // proxy's IP address as the value since it is always the same.
+        const remoteIP =
+          req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
-    // Get recaptcha token passed in as header (express lowercases all headers)
-    const token = req.headers["x-recaptcha-token"];
+        // Get recaptcha token passed in as header (express lowercases all headers)
+        const token = req.headers["x-recaptcha-token"];
 
-    if (token === undefined || token === "") {
-      throw new ForbiddenException("Missing recaptcha token");
-    }
+        if (token === undefined || token === "") {
+          throw new ForbiddenException("Missing recaptcha token");
+        }
 
-    const { err, res } = await sf
-      .useOnce(
-        `httpss://www.recaptcha.net/recaptcha/api/siteverify?secret=${recaptchaSecret}&response=${token}&remoteip=${remoteIP}`
-      )
-      .POST()
-      .runJSON<{ success: string; score: number; ["error-codes"]: any }>();
+        const { err, res } = await sf
+          .useOnce(
+            `httpss://www.recaptcha.net/recaptcha/api/siteverify?secret=${recaptchaSecret}&response=${token}&remoteip=${remoteIP}`
+          )
+          .POST()
+          .runJSON<{ success: string; score: number; ["error-codes"]: any }>();
 
-    if (err !== undefined) {
-      throw new Error(
-        `Failed to verify recaptcha token: ${err.name}: ${err.message}`
-      );
-    }
+        if (err !== undefined) {
+          throw new Error(
+            `Failed to verify recaptcha token: ${err.name}: ${err.message}`
+          );
+        }
 
-    if (!res.data.success) {
-      throw new ForbiddenException(prettyPrintJson(res.data["error-codes"]));
-    }
+        if (!res.data.success) {
+          throw new ForbiddenException(
+            prettyPrintJson(res.data["error-codes"])
+          );
+        }
 
-    if (res.data.score < scoreRequirement) {
-      throw new ForbiddenException(
-        `Recaptcha score too low: ${res.data.score}`
-      );
-    }
-  } satisfies HttpRequestGuard;
+        if (res.data.score < scoreRequirement) {
+          throw new ForbiddenException(
+            `Recaptcha score too low: ${res.data.score}`
+          );
+        }
+      } satisfies HttpRequestGuard);
