7d091d5487c0d352974c3eac8fec0e54e5cf2f72
Jaime <jaimeloeuf@gmail.com>
2024-08-03 22:40:09 +0800

    Fix config error.

--- api/src/infra/Telegram/notifyAdminWithInternalAdminTelegramBot.ts
+++ api/src/infra/Telegram/notifyAdminWithInternalAdminTelegramBot.ts
@@ -1,9 +1,7 @@
 import { config } from "../../config/index.js";
 import { TelegramBotFF } from "./TelegramBot.js";
 
-const InternalAdminTelegramBot = TelegramBotFF(
-  config.tele_adminbot_admin_chat_id
-);
+const InternalAdminTelegramBot = TelegramBotFF(config.tele_adminbot_token);
 
 export const notifyAdminWithInternalAdminTelegramBot = (message: string) =>
   InternalAdminTelegramBot(config.tele_adminbot_admin_chat_id, message);
