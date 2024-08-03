3d4787526736bbfd97ac99078a8c092afea9edf3
Jaime <jaimeloeuf@gmail.com>
2024-08-04 01:03:03 +0800

    Test in prod.

--- api/src/services/blog/newSubscriber.ts
+++ api/src/services/blog/newSubscriber.ts
@@ -4,6 +4,8 @@ import { CreateSimplePostProcessingJob } from "../../post-processing/index.js";
 import { notifyAdminWithInternalAdminTelegramBot } from "../../infra/index.js";
 import { blogEmailService } from "./blogEmailService.js";
 
+import { logger } from "../../logging/index.js";
+
 /**
  * Add new subscriber for blog
  */
@@ -42,6 +44,7 @@ export async function newSubscriber(email: string) {
       );
 
       if (notificationError instanceof Error) {
+        logger.error(`Debugging tele error`, notificationError);
         throw notificationError;
       }
     })
