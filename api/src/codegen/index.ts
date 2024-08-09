2f18b5f0de0785062a03c074945d130ad276e66c
Jaime <jaimeloeuf@gmail.com>
2024-08-10 00:06:18 +0800

    Run codegen modules asynchronously:     Since codegen files should not depend on other codegen files, it is     ok for all of them to run asynchronously.

--- api/src/codegen/index.ts
+++ api/src/codegen/index.ts
@@ -5,7 +5,18 @@ import { genHttpRoutesTable } from "./genHttpRoutesTable/genHttpRoutesTable.js";
 async function codegenEntrypoint() {
   logger.info(codegenEntrypoint.name, "Running codegen");
 
-  await genHttpRoutesTable();
+  const codegenModulePromises = [
+    // List all codegen modules here manually
+    // Although they will all run concurrently, they will be called sequentially
+    genHttpRoutesTable(),
+  ];
+
+  await Promise.all(codegenModulePromises);
+
+  logger.info(
+    codegenEntrypoint.name,
+    `Codegen completed, ran ${codegenModulePromises.length} modules`
+  );
 }
 
 codegenEntrypoint();
