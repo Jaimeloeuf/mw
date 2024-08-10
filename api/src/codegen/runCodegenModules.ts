7185c3db9b81f2658deea5e8ccb57df8eb5764ae
Jaime <jaimeloeuf@gmail.com>
2024-08-11 02:29:54 +0800

    Fix codegen naming.

--- api/src/codegen/runCodegenModules.ts
+++ api/src/codegen/runCodegenModules.ts
@@ -2,15 +2,15 @@ import { logger } from "../logging/index.js";
 import type { CodegenFunction } from "./CodegenFunction.js";
 
 export async function runCodegenModules(
-  ...codegenModules: Array<CodegenFunction>
+  ...codegenFunctions: Array<CodegenFunction>
 ) {
   logger.info(runCodegenModules.name, "Running codegen");
 
   // Run all the codegen modules asynchronously since they MUST be independent
-  await Promise.all(codegenModules.map((codegenModule) => codegenModule()));
+  await Promise.all(codegenFunctions.map((codegenModule) => codegenModule()));
 
   logger.info(
     runCodegenModules.name,
-    `Codegen completed, ran ${codegenModules.length} modules`
+    `Codegen completed, ran ${codegenFunctions.length} modules`
   );
 }
