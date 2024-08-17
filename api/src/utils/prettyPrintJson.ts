b53a82efb0548e2e4bf477ef511b1e0f915b61c6
Jaime <jaimeloeuf@gmail.com>
2024-08-18 00:19:28 +0800

    Update prettyPrintJson to catch on throws.

--- api/src/utils/prettyPrintJson.ts
+++ api/src/utils/prettyPrintJson.ts
@@ -1,4 +1,21 @@
+import { logger } from "../logging/index.js";
+
 /**
- * Common way to pretty print JS Objects
+ * Common way to pretty print JS Objects.
+ *
+ * If stringification fails and JSON.stringify throws, it will be caught and the
+ * error will be logged, and an empty string will be returned instead.
  */
-export const prettyPrintJson = (obj: any) => JSON.stringify(obj, null, 2);
+export function prettyPrintJson(obj: any) {
+  try {
+    return JSON.stringify(obj, null, 2);
+  } catch (e) {
+    logger.error(
+      prettyPrintJson.name,
+      `JSON.stringify threw while trying to stringify: ${obj}`
+    );
+    logger.error(prettyPrintJson.name, `JSON.stringify threw: ${e}`);
+
+    return "";
+  }
+}
