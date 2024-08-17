0d9a60b51add8b2fc6f44b17abda7360ef5d724c
Jaime <jaimeloeuf@gmail.com>
2024-08-18 00:15:20 +0800

    Move error to exception conversion logic:     Move error to exception conversion logic out from ServiceException     constructor to errorToServiceException utility function, so that     other services can also use the ServiceException without having to     create a new Error just to use it.

--- api/src/exceptions/ServiceException.ts
+++ api/src/exceptions/ServiceException.ts
@@ -4,18 +4,8 @@ import { HttpTransformerableException } from "./HttpTransformerableException.js"
  * Generic HTTP transformable Service level Exception.
  */
 export class ServiceException extends HttpTransformerableException {
-  constructor(e: Error = new Error("Service Exception")) {
-    super(e.message);
-
-    this.name = e.name;
-
-    if (e.stack !== undefined) {
-      this.stack = e.stack;
-    }
-
-    if (e.cause !== undefined) {
-      this.cause = e.cause;
-    }
+  constructor(optionalMessage: string = "Service Exception") {
+    super(optionalMessage);
   }
 
   transformToHttpResponseData() {
--- api/src/exceptions/errorToServiceException.ts
+++ api/src/exceptions/errorToServiceException.ts
@@ -1,8 +1,20 @@
-import { ServiceException } from "src/exceptions/ServiceException.js";
+import { ServiceException } from "../exceptions/ServiceException.js";
 
 /**
  * Converts an Error into a generic HTTP transformable Service level Exception.
  */
 export function errorToServiceException(e: Error) {
-  return new ServiceException(e);
+  const exception = new ServiceException(e.message);
+
+  exception.name = e.name;
+
+  if (e.stack !== undefined) {
+    exception.stack = e.stack;
+  }
+
+  if (e.cause !== undefined) {
+    exception.cause = e.cause;
+  }
+
+  return exception;
 }
