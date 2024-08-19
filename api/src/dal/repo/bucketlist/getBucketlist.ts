e8c888bb415f2ace22d4d5fb31184413628049b2
Jaime <jaimeloeuf@gmail.com>
2024-08-19 22:17:12 +0800

    Update getBucketlist DAL function to use concurrent.

--- api/src/dal/repo/bucketlist/getBucketlist.ts
+++ api/src/dal/repo/bucketlist/getBucketlist.ts
@@ -1,20 +1,21 @@
 import { db } from "../../kysely/index.js";
 import { NotFoundException } from "../../../exceptions/index.js";
+import { concurrent } from "../../../utils/index.js";
 
 export async function getBucketlist(id: string) {
-  const [bucketlist, bucketlistItems] = await Promise.all([
-    db
+  const { bucketlist, bucketlistItems } = await concurrent({
+    bucketlist: db
       .selectFrom("bucketlist")
       .selectAll()
       .where("id", "=", id)
       .executeTakeFirst(),
 
-    db
+    bucketlistItems: db
       .selectFrom("bucketlist_item")
       .selectAll()
       .where("bucketlist_id", "=", id)
       .execute(),
-  ]);
+  });
 
   if (bucketlist === undefined) {
     throw new NotFoundException(`Cannot find bucketlist ${id}`);
