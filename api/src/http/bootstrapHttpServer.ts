754abc4790fca5865001cb60174e54a5dbecb2c8
Jaime <jaimeloeuf@gmail.com>
2024-08-25 00:04:37 +0800

    Only use builder methods in bootstrapHttpServer.

--- api/src/http/bootstrapHttpServer.ts
+++ api/src/http/bootstrapHttpServer.ts
@@ -14,46 +14,49 @@ import { routeNotFound } from "./routeNotFound.js";
  * HTTP controllers.
  */
 export function bootstrapHttpServer() {
-  const app = express();
-
-  app
+  express()
     /* Register all the middlewares */
     .use(cors())
     .use(loggingMiddleware)
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
+
     // Register all the route->controller mappings with the /api prefix.
-    .use("/api", registerRoutesAndControllers());
+    .use("/api", registerRoutesAndControllers())
 
-  // Since this is the last non-error-handling route handler used, assume 404
-  // as no other route handler responded.
-  app.use(routeNotFound.routeHandler);
+    // Since this is the last non-error-handling route handler used, assume 404
+    // as no other route handler responded.
+    .use(routeNotFound.routeHandler)
 
-  const server = app.listen(config.port, () => {
-    logger.info(
-      bootstrapHttpServer.name,
-      `Web Server now listening on port: ${config.port}`
-    );
-  });
+    // Start listening to network port and returns the created server instance.
+    .listen(config.port, () => {
+      logger.info(
+        bootstrapHttpServer.name,
+        `Web Server now listening on port: ${config.port}`
+      );
 
-  // Note that setting timeout does not stop any in progress controller/service/
-  // post-processing execution. All it does is hangup on the socket connected to
-  // the client and logs it as an error.
-  server.setTimeout(config.server_timeout);
-  server.on("timeout", (socket: Socket) => {
-    socket.destroy();
+      logger.info(
+        bootstrapHttpServer.name,
+        `Web Server global timeout set to: ${config.server_timeout} ms`
+      );
+    })
 
-    // This is an internal API that may or may not work.
-    const originalRequestUrl = (socket as any)?.parser?.incoming?.originalUrl;
+    // Set hard timeout to ensure that execution do not hog resources for long.
+    // Note that setting this does not stop any in progress controller/service/
+    // post-processing execution. All it does is hangup on the socket connected
+    // to the client and logs it as an error.
+    .setTimeout(config.server_timeout)
 
-    logger.error(
-      bootstrapHttpServer.name,
-      `Request to '${originalRequestUrl}' exceeded global timeout of ${config.server_timeout} ms`
-    );
-  });
+    // Event handler that runs after express forces a timeout set above.
+    .on("timeout", (socket: Socket) => {
+      socket.destroy();
 
-  logger.info(
-    bootstrapHttpServer.name,
-    `Web Server global timeout set to: ${config.server_timeout} ms`
-  );
+      // This is an internal API that may or may not work.
+      const originalRequestUrl = (socket as any)?.parser?.incoming?.originalUrl;
+
+      logger.error(
+        bootstrapHttpServer.name,
+        `Request to '${originalRequestUrl}' exceeded global timeout of ${config.server_timeout} ms`
+      );
+    });
 }
