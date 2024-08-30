c546ec56f6f65851d36166ca4b8dcf36687fd88c
Jaime <jaimeloeuf@gmail.com>
2024-08-31 00:05:48 +0800

    Fix generatedDocDirPath:     Path should be relative from the codegen runner which is /api/     instead of being relative to the file itself.

--- api/src/codegen/codegenForDoc/generatedDocDirPath.ts
+++ api/src/codegen/codegenForDoc/generatedDocDirPath.ts
@@ -1,6 +1,3 @@
 import path from "path";
 
-export const generatedDocDirPath = path.resolve(
-  import.meta.dirname,
-  "../../../docs",
-);
+export const generatedDocDirPath = path.resolve("../docs");
