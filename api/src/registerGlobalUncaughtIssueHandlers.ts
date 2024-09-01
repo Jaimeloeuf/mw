41aad967734c653d7371d1d2eeab674525cfa637
Jaime <jaimeloeuf@gmail.com>
2024-09-02 01:07:02 +0800

    Fix file formatting.

--- api/src/registerGlobalUncaughtIssueHandlers.ts
+++ api/src/registerGlobalUncaughtIssueHandlers.ts
@@ -5,18 +5,18 @@ export function registerGlobalUncaughtIssueHandlers() {
   process.on("unhandledRejection", (err: Error, promise: Promise<unknown>) => {
     logger.error(
       bootstrapHttpServer.name,
-      `Caught unhandled Rejection: ${promise}\n` + err.stack
+      `Caught unhandled Rejection: ${promise}\n` + err.stack,
     );
   });
   process.on("uncaughtException", (err: Error) => {
     logger.error(
       bootstrapHttpServer.name,
-      `Caught unhandled Exception: ${err}\n` + err.stack
+      `Caught unhandled Exception: ${err}\n` + err.stack,
     );
   });
 
   logger.verbose(
     registerGlobalUncaughtIssueHandlers.name,
-    "Registered global handlers for uncaught promise rejections and errors"
+    "Registered global handlers for uncaught promise rejections and errors",
   );
 }
