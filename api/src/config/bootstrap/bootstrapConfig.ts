dd925d8d84e19ee6ad195766c08f47e66add9357
Jaime <jaimeloeuf@gmail.com>
2024-09-03 00:04:44 +0800

    Update config to show all errors at once:     Instead of errors one by one, so that users can see all the issues     with config values at one shot before fixing them all.

--- api/src/config/bootstrap/bootstrapConfig.ts
+++ api/src/config/bootstrap/bootstrapConfig.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import { logger } from "../../logging/index.js";
 import { combinedConfig } from "../configMappings/combinedConfig.js";
+import { prettyPrintJson } from "../../utils/prettyPrintJson.js";
 import type { ConfigType } from "../ConfigType.js";
+import type { ZodIssue } from "zod";
 
 export function bootstrapConfig() {
   const config: Partial<ConfigType> = {};
+  const errors: Array<ZodIssue> = [];
 
   for (const configName in combinedConfig) {
     const configMapping =
@@ -18,6 +21,16 @@ export function bootstrapConfig() {
       process.exit(1);
     }
 
+    const { success, data, error } = configMapping.schema.safeParse(
+      configMapping.datasource,
+    );
+
+    if (!success) {
+      // Show both the error and what configName the error is for
+      errors.push(...error.errors.map((issue) => ({ configName, ...issue })));
+      continue;
+    }
+
     // @ts-ignore
     // Have to ts-ignore here in order for the type to work easily, see
     // https://github.com/microsoft/TypeScript/issues/32811
@@ -25,8 +38,13 @@ export function bootstrapConfig() {
     // https://www.totaltypescript.com/iterate-over-object-keys-in-typescript#solution-1-cast-to-keyof-typeof
     // https://stackoverflow.com/questions/72282500/for-in-loop-converting-typescript-types-to-intersection
     // https://stackoverflow.com/questions/61829651/how-can-i-iterate-over-record-keys-in-a-proper-type-safe-way
-    config[configName as keyof typeof combinedConfig] =
-      configMapping.schema.parse(configMapping.datasource);
+    config[configName as keyof typeof combinedConfig] = data;
+  }
+
+  if (errors.length > 0) {
+    logger.error(bootstrapConfig.name, `Failed to parse and initialise config`);
+    logger.error(bootstrapConfig.name, prettyPrintJson(errors));
+    process.exit(1);
   }
 
   return Object.freeze(config as ConfigType);
