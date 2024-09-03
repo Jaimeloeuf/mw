1c69922c4df383efd07ed7761cdab0659ef7f7c5
Jaime <jaimeloeuf@gmail.com>
2024-09-04 00:15:57 +0800

    Create muwno product table definition.

--- api/src/dal/kysely/definitions/DatabaseDefinition.ts
+++ api/src/dal/kysely/definitions/DatabaseDefinition.ts
@@ -3,6 +3,7 @@ import type { BucketlistTable } from "./BucketlistTable.js";
 import type { BucketlistItemTable } from "./BucketlistItemTable.js";
 import type { MuwnoOrgTable } from "./MuwnoOrgTable.js";
 import type { MuwnoUserTable } from "./MuwnoUserTable.js";
+import type { MuwnoProductTable } from "./MuwnoProductTable.js";
 
 export interface Database {
   blog_subscriber: BlogSubscriberTable;
@@ -10,4 +11,5 @@ export interface Database {
   bucketlist_item: BucketlistItemTable;
   muwno_org: MuwnoOrgTable;
   muwno_user: MuwnoUserTable;
+  muwno_product: MuwnoProductTable;
 }
---
+++ api/src/dal/kysely/definitions/MuwnoProductTable.ts
@@ -0,0 +1,37 @@
+import type { ColumnType, Insertable, Selectable, Updateable } from "kysely";
+
+export interface MuwnoProductTable {
+  /**
+   * Unique opaque ID that cannot be updated
+   */
+  id: ColumnType<string, string, never>;
+
+  /**
+   * Track when was the Product created
+   */
+  created_at: ColumnType<Date, string | undefined, never>;
+
+  /**
+   * The product's Org ID.
+   */
+  org_id: string;
+
+  /**
+   * Product's name
+   */
+  name: string;
+
+  /**
+   * Optional link to the customer's product page
+   */
+  link: null | string;
+
+  /**
+   * Product's description
+   */
+  description: string;
+}
+
+export type MuwnoProduct = Selectable<MuwnoProductTable>;
+export type CreateMuwnoProduct = Insertable<MuwnoProductTable>;
+export type UpdateMuwnoProduct = Updateable<MuwnoProductTable>;
