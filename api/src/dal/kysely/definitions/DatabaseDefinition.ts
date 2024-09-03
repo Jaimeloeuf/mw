fa6dd3ccb3710374faf74a9e4b03ee0d84036855
Jaime <jaimeloeuf@gmail.com>
2024-09-03 23:56:43 +0800

    Create muwno org table definition.

--- api/src/dal/kysely/definitions/DatabaseDefinition.ts
+++ api/src/dal/kysely/definitions/DatabaseDefinition.ts
@@ -1,9 +1,11 @@
 import type { BlogSubscriberTable } from "./BlogSubscriberTable.js";
 import type { BucketlistTable } from "./BucketlistTable.js";
 import type { BucketlistItemTable } from "./BucketlistItemTable.js";
+import type { MuwnoOrgTable } from "./MuwnoOrgTable.js";
 
 export interface Database {
   blog_subscriber: BlogSubscriberTable;
   bucketlist: BucketlistTable;
   bucketlist_item: BucketlistItemTable;
+  muwno_org: MuwnoOrgTable;
 }
---
+++ api/src/dal/kysely/definitions/MuwnoOrgTable.ts
@@ -0,0 +1,58 @@
+import type { ColumnType, Insertable, Selectable, Updateable } from "kysely";
+
+export interface MuwnoOrgTable {
+  /**
+   * Unique opaque ID that cannot be updated
+   */
+  id: ColumnType<string, string, never>;
+
+  /**
+   * Track when was the Org created
+   */
+  created_at: ColumnType<Date, string | undefined, never>;
+
+  /**
+   * Name of the Org
+   */
+  name: string;
+
+  /**
+   * Main administrative email for the Org
+   */
+  email: string;
+
+  /**
+   * Phone number stored as string to accomodate all types of numbers
+   */
+  phone: string;
+
+  /**
+   * Arbitrary string for address
+   */
+  address: null | string;
+
+  /**
+   * Arbitrary string for Org size
+   */
+  size: null | string;
+
+  /**
+   * Did the Org pass manual human verification by the muwno team?
+   */
+  verified: boolean;
+
+  /**
+   * Is the Org a subscribed paying customer?
+   */
+  subscribed: boolean;
+
+  /**
+   * The Org's subscription plan.
+   * This is optional as Orgs are created before they pay for a plan.
+   */
+  subscription_plan: null | "Standard";
+}
+
+export type MuwnoOrg = Selectable<MuwnoOrgTable>;
+export type CreateMuwnoOrg = Insertable<MuwnoOrgTable>;
+export type UpdateMuwnoOrg = Updateable<MuwnoOrgTable>;
