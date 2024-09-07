2e03459d05e71cb5f50a30537a46189fe1417373
Jaime <jaimeloeuf@gmail.com>
2024-09-08 00:54:30 +0800

    Export all DB table/rows type definitions.

--- api/src/dal/kysely/definitions/index.ts
+++ api/src/dal/kysely/definitions/index.ts
@@ -3,6 +3,18 @@
  */
 
 export * from "./DatabaseDefinition.js";
+
 export * from "./BlogSubscriberTable.js";
 export * from "./BucketlistTable.js";
 export * from "./BucketlistItemTable.js";
+export * from "./MuwnoApiKeyTable.js";
+export * from "./MuwnoCustomerTable.js";
+export * from "./MuwnoOrgTable.js";
+export * from "./MuwnoPmfSurveyResponseTable.js";
+export * from "./MuwnoProductTable.js";
+export * from "./MuwnoStripeCustomerTable.js";
+export * from "./MuwnoStripeSetupNextTable.js";
+export * from "./MuwnoStripeWebhookEventTable.js";
+export * from "./MuwnoTaskTable.js";
+export * from "./MuwnoTeamMemberInvitationTable.js";
+export * from "./MuwnoUserTable.js";
