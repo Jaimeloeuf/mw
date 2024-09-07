d4fb23c026dbaf0c66c8eb14f387b1450c806557
Jaime <jaimeloeuf@gmail.com>
2024-09-08 01:06:44 +0800

    Port muwno isUserOnboarded dal method.

--- api/src/dal/repo/muwno/index.ts
+++ api/src/dal/repo/muwno/index.ts
@@ -1,10 +1,20 @@
 import { dalWrapper } from "../dalWrapper.js";
 
 import { getUser } from "./getUser.js";
+import { isUserOnboarded } from "./isUserOnboarded.js";
 
 export const muwnoRepo = {
   /**
    * Get User Entity object, throws `NotFoundException` if not found.
    */
   getUser: dalWrapper(getUser),
+
+  /**
+   * Check if given user completed onboarding.
+   *
+   * To be considered onboarded
+   * 1. User must have an Org
+   * 2. The org must have a valid subscription
+   */
+  isUserOnboarded: dalWrapper(isUserOnboarded),
 };
---
+++ api/src/dal/repo/muwno/isUserOnboarded.ts
@@ -0,0 +1,35 @@
+import { apiDB } from "../../kysely/index.js";
+import {
+  NotFoundException,
+  InvalidInternalStateException,
+} from "../../../exceptions/index.js";
+
+export async function isUserOnboarded(userID: string) {
+  const user = await apiDB
+    .selectFrom("muwno_user")
+    .select("org_id")
+    .where("id", "=", userID)
+    .executeTakeFirst();
+
+  if (user === undefined) {
+    throw new NotFoundException(`Cannot find user: ${userID}`);
+  }
+
+  if (user.org_id === null) {
+    return false;
+  }
+
+  const org = await apiDB
+    .selectFrom("muwno_org")
+    .select("subscribed")
+    .where("id", "=", user.org_id)
+    .executeTakeFirst();
+
+  if (org === undefined) {
+    throw new InvalidInternalStateException(
+      `User '${userID}' has an invalid org_id '${user.org_id}'`,
+    );
+  }
+
+  return org.subscribed;
+}
