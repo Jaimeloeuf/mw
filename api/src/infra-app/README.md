45cda5fdf2a32f8e0912942e9ec1d5fb6e83ee34
Jaime <jaimeloeuf@gmail.com>
2024-09-22 01:51:57 +0800

    Port muwno OpenAiService.

---
+++ api/src/infra-app/README.md
@@ -0,0 +1,4 @@
+# api/src/infra-app/
+App specific infra services that isnt part of core business logic.
+
+The infra services are grouped by their app names.
---
+++ api/src/infra-app/muwno/OpenAi/OpenAi.ts
@@ -0,0 +1,84 @@
+import OpenAI from "openai";
+import {
+  listOfNoFeedbackStringsMaxLength,
+  listOfNoFeedbackStrings,
+} from "./hardCodedNoFeedbackStrings.js";
+
+/**
+ * @todo implement rate limiting and batching
+ * https://platform.openai.com/docs/guides/rate-limits/overview
+ */
+export class OpenAi {
+  /**
+   * Hold the `OpenAIApi` client after creating it in constructor.
+   */
+  private readonly openai: OpenAI;
+
+  constructor(config: { organization: string; apiKey: string }) {
+    this.openai = new OpenAI(config);
+  }
+
+  // @todo Handle errors once this uses OpenAI API for internal logging to monitor when things go wrong
+  async isInvalidCustomerFeedback(customerFeedback: string) {
+    // Simple optimisation to decide if the next check should be performed
+    // against the list of hardcoded no feedback strings, by checking if length
+    // of customer feedback is larger than `listOfNoFeedbackStringsMaxLength`
+    // because if the feedback is a longer text, then it is impossible for it to
+    // appear in the list of hardcoded no feedback strings. By skipping this it
+    // means that this will not waste CPU cycles iterating and checking through
+    // super long customer feedbacks.
+    // Return false to indicate that the customer feedback is valid.
+    if (customerFeedback.length > listOfNoFeedbackStringsMaxLength)
+      return false;
+
+    // Check if feedback is any of the hardcoded words that means no feedback.
+    // If it matches any of the hardcoded strings, return true to indicate that
+    // the customer feedback is invalid.
+    return listOfNoFeedbackStrings.includes(
+      customerFeedback.trim().toLowerCase(),
+    );
+  }
+
+  // @todo Handle errors for internal logging to monitor when things go wrong
+  async getActionableTask(validatedCustomerFeedback: string) {
+    const response = await this.openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+
+      // After experimenting, it seems like sending everything in 1 shot works
+      // best with the user role, because the pre-empting the system role always
+      // causes it to give point form actionable insights instead.
+      messages: [
+        {
+          role: "user",
+          content: `You are a project and product manager, reword the given customer feedback into a single short sentence describing an actionable task that is easy to understand without changing its meaning. This is the customer feedback: "${validatedCustomerFeedback}"`,
+        },
+
+        // Alternative prompt that seem to mess up sometimes.
+        // {
+        //   role: 'system',
+        //   content: `You are a project and product manager, I will give you a customer feedback for you to rewrite into a single short sentence describing an actionable task that is easy to understand without changing its original meaning.`,
+        // },
+        // {
+        //   role: 'user',
+        //   content: `This is the customer feedback: "${validatedCustomerFeedback}"`,
+        // },
+      ],
+
+      // Using a temperature of 0 so that the generated response is the most
+      // deterministic so that we can reproduce it to a certain extent if needed.
+      temperature: 0,
+
+      // Only allow 1 response since only 1 task is saved
+      n: 1,
+    });
+
+    /**
+     * Get the first generated choice as the actionable task, and assume that it
+     * is always available since the API call explicitly request for exactly 1
+     * response to be generated.
+     */
+    const actionableTask = response.choices[0]?.message?.content ?? "";
+
+    return actionableTask;
+  }
+}
