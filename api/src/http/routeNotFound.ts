4ae01ca493d09277ec5038648699bf014953282d
Jaime <jaimeloeuf@gmail.com>
2024-10-13 20:05:26 +0800

    Simplify route not found logging.

--- api/src/http/routeNotFound.ts
+++ api/src/http/routeNotFound.ts
@@ -11,6 +11,18 @@ export const routeNotFound = httpController({
   urlQueryParamsValidator: null,
   requestBodyValidator: null,
   httpRequestHandler() {
-    throw new NotFoundException("API Route not found");
+    const errMsg = "API Route not found";
+    const err = new NotFoundException(errMsg);
+
+    // Replace the stack trace with just the error message.
+    // In prod, theres a lot of spam/fuzzy calls to find endpoints for
+    // vulnerabilities, and since 404 is throw as an exception, the whole stack
+    // trace is printed out, which clutters the logs and makes it hard to find
+    // things we actually care about. So this just let things stay the same
+    // except now with a 1 line stack trace.
+    delete err.stack;
+    err.stack = errMsg;
+
+    throw err;
   },
 });
