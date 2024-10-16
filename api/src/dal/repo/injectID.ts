041d0e4b51eb1e22c5370ebc8c4edd06f2780266
Jaime <jaimeloeuf@gmail.com>
2024-10-16 23:02:41 +0800

    Create injectID utility:     So that we can move DB ID generation to DAL instead of doing it in     service functions.

---
+++ api/src/dal/repo/injectID.ts
@@ -0,0 +1,24 @@
+/**
+ * Utility type to make the specified key in an object optional.
+ */
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+/**
+ * Utility type to make the `id` key of an object optional.
+ */
+export type OptionalID<T extends { id: any }> = Optional<T, "id">;
+
+/**
+ * Utility function to generate and inject an `id` field into an object with a
+ * possibly optional `id`. Does nothing if the `id` field is not null/undefined.
+ */
+export function injectID<T extends { id: any }>(
+  objectWithoutID: OptionalID<T>,
+): T {
+  // If ID property not equals to null/undefined, assume it is supplied
+  if (objectWithoutID["id"] != null) {
+    return objectWithoutID as T;
+  }
+  objectWithoutID["id"] = crypto.randomUUID();
+  return objectWithoutID as T;
+}
