0903b173a73cd1f1899e2d0e2455d00b1918cd86
Jaime <jaimeloeuf@gmail.com>
2024-10-27 03:24:58 +0800

    Refactor out single codegen function loading logic.

--- api/src/codegen/codegen-runner/codegenRunSingleModule.ts
+++ api/src/codegen/codegen-runner/codegenRunSingleModule.ts
@@ -1,10 +1,9 @@
 import fs from "fs";
 import path from "path";
-import { tsImport } from "tsx/esm/api";
 import { logger } from "../../logging/index.js";
+import { loadCodegenFunction } from "./loadCodegenFunction.js";
 import { runCodegenModules } from "./runCodegenModules.js";
 import { genCodegenBarrelFile } from "../codegenForTs/index.js";
-import type { CodegenFunction } from "./CodegenFunction.js";
 
 export async function codegenRunSingleModule(codegenModuleName: string) {
   // Codegen module should be in this codegen/ folder, and should be in a folder
@@ -29,24 +28,10 @@ export async function codegenRunSingleModule(codegenModuleName: string) {
     return;
   }
 
-  // Cannot use `import` directly since it does not support loading .ts files
-  const importedModule = await tsImport(codegenModulePath, import.meta.url);
-
-  // Extract out the named export and make sure it is a function
-  const importedCodegenFunction = importedModule[
-    codegenModuleName
-  ] as CodegenFunction;
-  if (typeof importedCodegenFunction !== "function") {
-    logger.error(
-      codegenRunSingleModule.name,
-      `Codegen module is malformed and does not export codegen function\nMake sure they are of the same name`,
-    );
-    logger.error(
-      codegenRunSingleModule.name,
-      `Module found: ${importedModule}`,
-    );
-    return;
-  }
+  const importedCodegenFunction = await loadCodegenFunction(
+    codegenModuleName,
+    codegenModulePath,
+  );
 
   // For single module codegen, since we are unable to know if the output file
   // path has changed, we must rely on users to manually delete it as we cannot
---
+++ api/src/codegen/codegen-runner/loadCodegenFunction.ts
@@ -0,0 +1,29 @@
+import { tsImport } from "tsx/esm/api";
+import { logger } from "../../logging/index.js";
+import type { CodegenFunction } from "./CodegenFunction.js";
+
+/**
+ * Dynamically load and return the given codegen function.
+ */
+export async function loadCodegenFunction(
+  codegenModuleName: string,
+  codegenModulePath: string,
+) {
+  // Cannot use `import` directly since it does not support loading .ts files
+  const importedModule = await tsImport(codegenModulePath, import.meta.url);
+
+  // Extract out the named export and make sure it is a function
+  const importedCodegenFunction: CodegenFunction =
+    importedModule[codegenModuleName];
+
+  if (typeof importedCodegenFunction !== "function") {
+    logger.error(
+      loadCodegenFunction.name,
+      `Codegen module is malformed and does not export codegen function\nMake sure they are of the same name`,
+    );
+    logger.error(loadCodegenFunction.name, `Module found: ${importedModule}`);
+    process.exit(1);
+  }
+
+  return importedCodegenFunction;
+}
