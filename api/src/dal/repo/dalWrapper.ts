8586e11fcf8ce16be82820e423ad7b670557969d
Jaime <jaimeloeuf@gmail.com>
2024-10-29 00:04:44 +0800

    Re-implement dalWrapper using noThrow.

--- api/src/dal/repo/dalWrapper.ts
+++ api/src/dal/repo/dalWrapper.ts
@@ -1,5 +1,5 @@
 import { logger } from "../../logging/index.js";
-import { unknownCatchToError } from "../../utils/index.js";
+import { noThrow } from "../../utils/index.js";
 
 /**
  * DAL function wrapper to wrap them and provide common utility features such as
@@ -7,7 +7,10 @@ import { unknownCatchToError } from "../../utils/index.js";
  * of either calling the DAL function and let errors/exceptions bubble through
  * or catch all errors/exceptions and return it for manual handling.
  */
-export function dalWrapper<T extends (...args: any) => Promise<any>>(fn: T) {
+export function dalWrapper<
+  T extends (...args: any) => Promise<any>,
+  Result extends Awaited<ReturnType<T>> = Awaited<ReturnType<T>>,
+>(fn: T) {
   // Extra runtime check alongside compile time check with ESLint rule
   // 'mwEslintPlugin/require-function-name-for-dalWrapper'
   if (fn.name === "") {
@@ -15,41 +18,32 @@ export function dalWrapper<T extends (...args: any) => Promise<any>>(fn: T) {
   }
 
   /**
-   * Run DAL repo function and catches any error/exceptions encountered to
-   * return it. This means the return type will always be a union of the
-   * original DAL function's return type and Error.
+   * Run DAL repo function and catches any error/exceptions encountered using
+   * `noThrow`. Which means the return type will always be a tuple of an error
+   * and the original DAL function's return type, which you can use to type
+   * narrow down to either a failure or success case.
    *
    * If you need to do some action like say, send user an email notification
    * about the failed service function call, use this to get the error
    * / exception back and handle it yourself manually alongside any action
    * you want to run.
    *
-   * If you want service function to stop executing on DAL errors/exceptions,
+   * If you want service function to stop executing on DAL errors/exceptions
    * and do not need to do any extra action in service function after the
-   * error/exception, then instead of manually checking if result is `Error`
-   * before re-throwing it, use the `getResultOrThrowOnError` method instead.
+   * error/exception (i.e. you just re-throw the error in the service layer),
+   * then instead of manually checking if dal method errored before re-throwing
+   * it, use the `getResultOrThrowOnError` method instead.
    */
   async function getResultOrError(
     ...args: Parameters<T>
-  ): Promise<Error | Awaited<ReturnType<T>>> {
-    try {
-      return await fn(...args);
-    } catch (e) {
-      // Convert unknown `e` type to a definite `Error` type before returning it
-      const error = unknownCatchToError(e);
+  ): Promise<[null, Result] | [Error, null]> {
+    const noThrowResult = await noThrow(() => fn(...args));
 
-      logger.error(
-        dalWrapper.name,
-        `Failed to execute DAL repo call: ${fn.name}`,
-      );
-
-      logger.error(
-        `${dalWrapper.name}:${fn.name}`,
-        error instanceof Error ? error.stack : error,
-      );
-
-      return error;
+    if (noThrowResult[0] !== null) {
+      logDalError(fn.name, noThrowResult[0]);
     }
+
+    return noThrowResult;
   }
 
   /**
@@ -64,23 +58,33 @@ export function dalWrapper<T extends (...args: any) => Promise<any>>(fn: T) {
    * function.
    *
    * If you need to do some action like say, send user an email notification
-   * about the failed service function call, use `getResultOrError` instead to
-   * get the error/exception back and handle it yourself manually.
+   * about the failed dal method call, use `getResultOrError` instead to get the
+   * error/exception back and handle it yourself manually.
    */
-  async function getResultOrThrowOnError(...args: Parameters<T>) {
-    // Still call the wrapped function to ensure that unknown catch types are
-    // converted to `Error` type and error logging is ran first.
-    const result = await getResultOrError(...args);
+  async function getResultOrThrowOnError(
+    ...args: Parameters<T>
+  ): Promise<Result> {
+    const noThrowResult = await noThrow(() => fn(...args));
 
-    if (result instanceof Error) {
-      throw result;
+    if (noThrowResult[0] !== null) {
+      logDalError(fn.name, noThrowResult[0]);
+      throw noThrowResult[0];
     }
 
-    return result;
+    return noThrowResult[1];
   }
 
   return {
     getResultOrError,
     getResultOrThrowOnError,
   };
+}
+
+/**
+ * Log DAL errors in the same format regardless of whether caller choose to use
+ * `getResultOrError` or `getResultOrThrowOnError`.
+ */
+function logDalError(fnName: string, error: Error) {
+  logger.error(dalWrapper.name, `Failed to execute DAL repo call: ${fnName}`);
+  logger.error(`${dalWrapper.name}:${fnName}`, error?.stack ?? error);
 }
