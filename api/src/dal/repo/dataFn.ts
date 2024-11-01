0637fa0dea612f5fb73cf99aa962c0b7491e82ff
Jaime <jaimeloeuf@gmail.com>
2024-11-01 23:36:38 +0800

    Update dataFn docs and remove optional name.

--- api/src/dal/repo/dataFn.ts
+++ api/src/dal/repo/dataFn.ts
@@ -2,35 +2,25 @@ import { logger } from "../../logging/index.js";
 import { noThrowPromise } from "../../utils/index.js";
 
 /**
- * DAL function wrapper to wrap them and provide common utility features such as
- * error/exception logging, and to give DAL users (service functions) a choice
- * of either calling the DAL function and let errors/exceptions bubble through
- * or catch all errors/exceptions and return it for manual handling.
+ * Data Function wrapper to wrap them and provide common utility features such
+ * as error/exception logging, and to give DAL users (service functions) a
+ * choice of either calling the data function and let errors/exceptions bubble
+ * through or catch all errors/exceptions and return it for manual handling.
  */
 export function dataFn<
   T extends (...args: any) => Promise<any>,
   Result extends Awaited<ReturnType<T>> = Awaited<ReturnType<T>>,
->(
-  fn: T,
-
-  /**
-   * Data Function name
-   */
-  name?: string,
-) {
-  // @todo Delete the eslint check once dfName is fully used
+>(fn: T) {
   // Extra runtime check alongside compile time check with ESLint rule
   // 'mwEslintPlugin/require-function-name-for-dalWrapper'
-  // if (fn.name === "") {
-  //   throw new Error(`Functions passed to ${dalWrapper.name} must be named`);
-  // }
-
-  const dfName = name ?? fn.name;
+  if (fn.name === "") {
+    throw new Error(`Functions passed to ${dataFn.name} must be named`);
+  }
 
   /**
-   * Run DAL repo function and catches any error/exceptions encountered using
+   * Run data function and catches any error/exceptions encountered using
    * `noThrow`. Which means the return type will always be a tuple of an error
-   * and the original DAL function's return type, which you can use to type
+   * and the original data function's return type, which you can use to type
    * narrow down to either a failure or success case.
    *
    * If you need to do some action like say, send user an email notification
@@ -38,10 +28,10 @@ export function dataFn<
    * / exception back and handle it yourself manually alongside any action
    * you want to run.
    *
-   * If you want service function to stop executing on DAL errors/exceptions
+   * If you want service function to stop executing on data errors/exceptions
    * and do not need to do any extra action in service function after the
    * error/exception (i.e. you just re-throw the error in the service layer),
-   * then instead of manually checking if dal method errored before re-throwing
+   * then instead of manually checking if data method errored before re-throwing
    * it, use the `getResultOrThrowOnError` method instead.
    */
   async function getResultOrError(
@@ -50,26 +40,26 @@ export function dataFn<
     const noThrowResult = await noThrowPromise(fn(...args));
 
     if (noThrowResult[0] !== null) {
-      logDalError(dfName, noThrowResult[0]);
+      logDataFnError(fn.name, noThrowResult[0]);
     }
 
     return noThrowResult;
   }
 
   /**
-   * Run DAL repo function, and throws on any error/exceptions encountered.
+   * Run data function, and throws on any error/exceptions encountered.
    *
    * The primary purpose of this is so that service functions do not have to
    * deal with manually checking if result is `Error` before re-throwing it
    * themselves, and just let this method throw it for them instead.
    *
-   * Only use this if you want service function to stop executing on DAL
-   * errors/exceptions, and do not need to do any extra action in service
-   * function.
+   * Only use this if you want service function to stop executing on data
+   * function errors/exceptions, and do not need to do any extra action in
+   * service function.
    *
    * If you need to do some action like say, send user an email notification
-   * about the failed dal method call, use `getResultOrError` instead to get the
-   * error/exception back and handle it yourself manually.
+   * about the failed data function call, use `getResultOrError` instead to get
+   * the error/exception back and handle it yourself manually.
    */
   async function getResultOrThrowOnError(
     ...args: Parameters<T>
@@ -77,7 +67,7 @@ export function dataFn<
     const noThrowResult = await noThrowPromise(fn(...args));
 
     if (noThrowResult[0] !== null) {
-      logDalError(dfName, noThrowResult[0]);
+      logDataFnError(fn.name, noThrowResult[0]);
       throw noThrowResult[0];
     }
 
@@ -91,10 +81,10 @@ export function dataFn<
 }
 
 /**
- * Log DAL errors in the same format regardless of whether caller choose to use
- * `getResultOrError` or `getResultOrThrowOnError`.
+ * Log data function errors in the same format regardless of whether caller
+ * choose to use `getResultOrError` or `getResultOrThrowOnError`.
  */
-function logDalError(fnName: string, error: Error) {
-  logger.error(dataFn.name, `Failed to execute DAL repo call: ${fnName}`);
+function logDataFnError(fnName: string, error: Error) {
+  logger.error(dataFn.name, `Error thrown in data function: ${fnName}`);
   logger.error(`${dataFn.name}:${fnName}`, error?.stack ?? error);
 }
