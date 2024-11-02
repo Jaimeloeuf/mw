2170a4f1f34bdc55189b55cdd6e9856e833ff5eb
Jaime <jaimeloeuf@gmail.com>
2024-11-02 20:27:50 +0800

    Update dataFn eslint rule to expect same name as file.

--- api/eslint-rules/require-function-name-for-dataFn.cjs
+++ api/eslint-rules/require-function-name-for-dataFn.cjs
@@ -1,8 +1,11 @@
+const path = require("path");
+
 module.exports = {
   meta: {
     type: "problem",
     docs: {
-      description: "Ensure functions passed to 'dataFn' are named",
+      description:
+        "Ensure functions passed to 'dataFn' are named and match the file name",
     },
     fixable: "code",
     schema: [],
@@ -14,12 +17,29 @@ module.exports = {
           return;
         }
 
+        const fileName = path.basename(context.filename).replace(".df.ts", "");
+
         for (const item of node.arguments) {
           const dataFnName = item.name ?? item?.id?.name;
 
           if (dataFnName === undefined) {
             context.report({
+              node,
               message: `Function passed to 'dataFn' must be named`,
+              loc: {
+                start: node.callee.loc.end,
+                end: node.callee.loc.end,
+              },
+            });
+
+            continue;
+          }
+
+          // Implement the fix function
+          if (dataFnName !== fileName) {
+            context.report({
+              node,
+              message: `Function passed to 'dataFn' must have the same name as module name. Expected '${fileName}', found '${dataFnName}'`,
               loc: {
                 start: node.callee.loc.end,
                 end: node.callee.loc.end,
