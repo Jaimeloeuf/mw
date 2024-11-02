416cb4edcdb745503617d1776a2a7bd753e72d97
Jaime <jaimeloeuf@gmail.com>
2024-11-03 01:25:00 +0800

    Add set to support skipping files in barrel file.

--- api/src/codegen/codegenForTs/genCodegenBarrelFile.ts
+++ api/src/codegen/codegenForTs/genCodegenBarrelFile.ts
@@ -1,6 +1,7 @@
 import fs from "fs/promises";
 import { generatedSrcDirPath } from "./generatedSrcDirPath.js";
 import { genAndSaveGeneratedCode } from "./genAndSaveGeneratedCode.js";
+import { filesToSkipForBarrelFileCodegen } from "./filesToSkipForBarrelFileCodegen.js";
 
 /**
  * Generate barrel file for all generated files
@@ -13,7 +14,11 @@ export async function genCodegenBarrelFile() {
   const generatedCode =
     `// Barrel file\n` +
     filesInGeneratedFolder
-      .filter((dirent) => dirent.name.endsWith(".generated.ts"))
+      .filter(
+        (dirent) =>
+          dirent.name.endsWith(".generated.ts") &&
+          !filesToSkipForBarrelFileCodegen.has(dirent.name),
+      )
       // Sort by name alphabetically to always have them in a stable position
       .sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
       .map((dirent) => `export * from './${dirent.name.replace(".ts", ".js")}'`)
