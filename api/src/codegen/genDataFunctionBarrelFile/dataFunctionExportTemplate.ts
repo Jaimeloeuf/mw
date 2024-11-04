b4965a4cae51a4c6d76239d0aa25c8fd5b050dfb
Jaime <jaimeloeuf@gmail.com>
2024-11-04 11:31:36 +0800

    Add new codegen step for data function barrel file.

---
+++ api/src/codegen/genDataFunctionBarrelFile/dataFunctionExportTemplate.ts
@@ -0,0 +1,12 @@
+import path from "path";
+import { DataFunctionFile } from "../utils/getDataFunctionFiles/DataFunctionFile.js";
+
+export const dataFunctionExportTemplate = (
+  file: DataFunctionFile,
+  dalFolderPath: string,
+): string =>
+  `export { default as ${file.name} } from "../dal/repo/${path.relative(
+    dalFolderPath,
+    file.path.replace(".ts", ".js"),
+  )}";
+`;
