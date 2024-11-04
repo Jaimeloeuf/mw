eefc933cba8abfe58b3a765472e5dfb0365ee97c
Jaime <jaimeloeuf@gmail.com>
2024-11-04 11:02:50 +0800

    Remove filesToSkipForBarrelFileCodegen:     This does not work, because codegen modules are found, loaded and     ran dynamically using tsx library's tsImport function. Which means     that the modules run in a different module space, which means the     Set is unique across the runs, and genCodegenBarrelFile will always     read it as an empty set.

--- api/src/codegen/codegenForTs/genCodegenBarrelFile.ts
+++ api/src/codegen/codegenForTs/genCodegenBarrelFile.ts
@@ -1,6 +1,5 @@
 import fs from "fs/promises";
 import { generatedSrcDirPath } from "./generatedSrcDirPath.js";
-import { filesToSkipForBarrelFileCodegen } from "./filesToSkipForBarrelFileCodegen.js";
 import { genAndSaveGeneratedFile } from "../utils/genAndSaveGeneratedFile.js";
 import { genGeneratedNotice } from "./genGeneratedNotice.js";
 
@@ -15,11 +14,7 @@ export async function genCodegenBarrelFile() {
   const generatedCode =
     `// Barrel file\n` +
     filesInGeneratedFolder
-      .filter(
-        (dirent) =>
-          dirent.name.endsWith(".generated.ts") &&
-          !filesToSkipForBarrelFileCodegen.has(dirent.name),
-      )
+      .filter((dirent) => dirent.name.endsWith(".generated.ts"))
       // Sort by name alphabetically to always have them in a stable position
       .sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
       .map((dirent) => `export * from './${dirent.name.replace(".ts", ".js")}'`)
