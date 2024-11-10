7fb242297de9acadbc7000444783fccc14e4be4c
Jaime <jaimeloeuf@gmail.com>
2024-11-11 00:57:14 +0800

    Fix bug caused by sorting http barrel file.

--- api/src/http/index.ts
+++ api/src/http/index.ts
@@ -2,9 +2,11 @@
  * Barrel file
  */
 
-export * from "./bootstrapHttpServer.js";
+// Exports need to be in this order for bootstrapping to work!
+/* eslint-disable perfectionist/sort-exports */
 export * from "./httpController.js";
+export * from "./useHttpRequestGuard.js";
+export * from "./JSend.js";
 export * from "./HttpRequestGuard.js";
 export * from "./HttpRequestGuardFF.js";
-export * from "./JSend.js";
-export * from "./useHttpRequestGuard.js";
+export * from "./bootstrapHttpServer.js";
