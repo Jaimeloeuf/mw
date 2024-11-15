64cd883ae7a46504fcaf807e3f7c14e259045b16
Jaime <jaimeloeuf@gmail.com>
2024-11-15 23:51:23 +0800

    Add docs to noThrowPromise.

--- api/src/utils/noThrowPromise.ts
+++ api/src/utils/noThrowPromise.ts
@@ -5,6 +5,18 @@ import { unknownCatchToError } from "./unknownCatchToError.js";
  *
  * **If you need to wrap an async function instead, use `noThrowFunction`**
  *
+ * ## Important Note
+ * Make sure you do not pass in a synchronous function call like
+ * ```typescript
+ * function test() {
+ *     throw new Error('testing');
+ * }
+ * noThrowPromise(test());
+ * ```
+ * This will not work, as it will throw before control flow even passes to
+ * `noThrowPromise`, effectively making this wrapping useless.
+ *
+ * ## Return Type
  * This will always return a 2 element tuple, whose type is
  * `[err, null] | [null, result]`. Similar to Go error handling, where if there
  * is an error the first element will be set to it and the second element will
