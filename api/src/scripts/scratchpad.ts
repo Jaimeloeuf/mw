b67adda4548229aa35a8b0e249abc23f4f4583d0
Jaime <jaimeloeuf@gmail.com>
2024-11-22 02:53:45 +0800

    Add scratchpad script.

---
+++ api/src/scripts/scratchpad.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+
+import { lazyConfig } from "../config/lazyConfig.js";
+
+/**
+ * `npm run scratchpad`
+ *
+ * Use this to quickly test out ideas like a personal scratch pad, but do not
+ * commit any changes to this into source control.
+ */
+async function scratchpad() {
+  lazyConfig.env;
+}
+
+scratchpad();
