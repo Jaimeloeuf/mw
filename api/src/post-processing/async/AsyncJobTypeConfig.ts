6467e4272c56ec59f5a06e25e3abddcd3ba191c4
Jaime <jaimeloeuf@gmail.com>
2024-12-02 01:36:56 +0800

    Add type for AsyncJobTypeConfig.

---
+++ api/src/post-processing/async/AsyncJobTypeConfig.ts
@@ -0,0 +1,51 @@
+import type { ZodType } from "zod";
+
+import type { AsyncJobMachineType } from "./AsyncJobMachineType.js";
+import type { AsyncJobPriority } from "./AsyncJobPriority.js";
+import type { JobResult } from "./JobResult.js";
+
+/**
+ * Config object for an Async Job Type.
+ */
+export type AsyncJobTypeConfig<
+  /**
+   * Generic type for the Job function's argument.
+   */
+  AsyncJobArgumentType = void,
+> = {
+  /**
+   * Unique ID for this Job Type.
+   *
+   * ## Why do we need an ID?
+   * Because a Job Type name can change over time but all previous async Jobs
+   * should still point back to the same Job Type.
+   */
+  id: string;
+
+  /**
+   * Job Type name used to display to users as a human readable string.
+   */
+  name: string;
+
+  /**
+   * Default Machine Type for Jobs of this Job Type.
+   */
+  machineType: AsyncJobMachineType;
+
+  /**
+   * Default Priority level for Jobs of this Job Type.
+   */
+  priority: AsyncJobPriority;
+
+  /**
+   * Optional `Zod` Validator to validate job arguments.
+   */
+  argumentValidator?: AsyncJobArgumentType extends void
+    ? void
+    : ZodType<Exclude<AsyncJobArgumentType, void>>;
+
+  /**
+   * The actual job function definition.
+   */
+  run(argument: AsyncJobArgumentType): JobResult | Promise<JobResult>;
+};
