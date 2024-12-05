8f0673c1e047e8ff74495d74624114156d03ce6a
Jaime <jaimeloeuf@gmail.com>
2024-12-06 00:02:51 +0800

    Add getStackTrace utility function.

---
+++ api/src/utils/getStackTrace.ts
@@ -0,0 +1,24 @@
+import { prettyPrintJson } from "./prettyPrintJson.js";
+
+/**
+ * Get stack trace in JSON string format
+ *
+ * ## Details
+ * Uses the `Error` stack to get the current stack trace, before we process it
+ * and JSON stringify it.
+ */
+export const getStackTrace = (numberOfStackFramesToDrop: number = 0) =>
+  prettyPrintJson(
+    Error()
+      .stack?.split("\n")
+      // Always drop at least 1 line, since this isnt part of the stack frames,
+      // but rather the word 'Error'.
+      .slice(1 + numberOfStackFramesToDrop)
+      .map((s) =>
+        s
+          // Remove the tab indentation
+          .trim()
+          // Remove 'at ' at the start to only keep symbol name and location
+          .replace("at ", ""),
+      ),
+  );
