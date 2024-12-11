36c01ad0786ff454a2abda58e0a4d45b6697ffcf
Jaime <jaimeloeuf@gmail.com>
2024-12-12 00:36:13 +0800

    Allow db connection check logging to be turned off.

--- api/src/dal/kysely/dbConnectionCheck.ts
+++ api/src/dal/kysely/dbConnectionCheck.ts
@@ -6,20 +6,26 @@ import { logger } from "../../logging/index.js";
 
 /**
  * Connection check function that runs a no-op query against the DB to check if
- * connection can be made to the DB. This will log the check results and return
- * boolean status.
+ * connection can be made to the DB, and return boolean status.
  */
-export async function dbConnectionCheck(db: Kysely<Database>) {
+export async function dbConnectionCheck(
+  db: Kysely<Database>,
+  logConnectionCheckResults: boolean = true,
+) {
   try {
     // No-Op query to check connection. This only works for PostgreSQL
     // https://stackoverflow.com/questions/3668506/efficient-sql-test-query-or-validation-query-that-will-work-across-all-or-most
     await sql`SELECT 1;`.execute(db);
 
-    logger.info(dbConnectionCheck.name, `DB connection check PASSED`);
+    if (logConnectionCheckResults) {
+      logger.info(dbConnectionCheck.name, `DB connection check PASSED`);
+    }
 
     return true;
   } catch (error) {
-    logger.error(dbConnectionCheck.name, `DB connection check FAILED`, error);
+    if (logConnectionCheckResults) {
+      logger.error(dbConnectionCheck.name, `DB connection check FAILED`, error);
+    }
 
     return false;
   }
