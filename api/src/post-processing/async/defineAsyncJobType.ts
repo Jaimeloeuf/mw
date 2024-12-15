48413a3327fb1a96198121729a3135fd1c322183
Jaime <jaimeloeuf@gmail.com>
2024-12-16 00:35:08 +0800

    Refactor out function.

--- api/src/post-processing/async/defineAsyncJobType.ts
+++ api/src/post-processing/async/defineAsyncJobType.ts
@@ -3,10 +3,10 @@ import type { AsyncJobType } from "./AsyncJobType.js";
 import type { AsyncJobTypeConfig } from "./AsyncJobTypeConfig.js";
 
 import { df } from "../../__generated/index.js";
-import { InvalidInternalStateException } from "../../exceptions/index.js";
 import { logger } from "../../logging/index.js";
 import { getStackTrace } from "../../utils/index.js";
 import { AsyncJobStatus } from "./AsyncJobStatus.js";
+import { validateJobArgumentOnSave } from "./validateJobArgumentOnSave.js";
 
 /**
  * Function to define an `AsyncJobType` object and creating the `scheduleJob`
@@ -23,7 +23,7 @@ export function defineAsyncJobType<T>(
      * Use this to schedule a new Job of this Job Type.
      */
     async scheduleJob(runOptions) {
-      const validatedJobArgument = validateJobArgument(
+      const validatedJobArgument = validateJobArgumentOnSave(
         asyncJobConfig,
         runOptions?.jobArguments,
       );
@@ -60,34 +60,4 @@ export function defineAsyncJobType<T>(
       );
     },
   };
-}
-
-/**
- * Validate Job arguments using the zod validator if any, if valid, return the
- * parsed argument.
- *
- * If arguments is invalid an error will be thrown.
- *
- * If there is no argumentValidator given, assume that the job does not take any
- * arguments and returns null instead.
- */
-function validateJobArgument<T>(
-  asyncJobConfig: AsyncJobTypeConfig<T>,
-  jobArguments?: unknown,
-) {
-  if (asyncJobConfig.argumentValidator === undefined) {
-    return null;
-  }
-
-  const { success, data, error } =
-    asyncJobConfig.argumentValidator.safeParse(jobArguments);
-
-  if (!success) {
-    throw new InvalidInternalStateException(
-      `Invalid Async Job arguments for '${asyncJobConfig.name}' ` +
-        error.toString(),
-    );
-  }
-
-  return data;
 }
---
+++ api/src/post-processing/async/validateJobArgumentOnSave.ts
@@ -0,0 +1,32 @@
+import { InvalidInternalStateException } from "../../exceptions/InvalidInternalStateException.js";
+import type { AsyncJobTypeConfig } from "./AsyncJobTypeConfig.js";
+
+/**
+ * Validate Job arguments using the zod validator if any, if valid, return the
+ * parsed argument.
+ *
+ * If arguments is invalid an error will be thrown.
+ *
+ * If there is no argumentValidator given, assume that the job does not take any
+ * arguments and returns null instead.
+ */
+export function validateJobArgumentOnSave<T>(
+  asyncJobConfig: AsyncJobTypeConfig<T>,
+  jobArguments?: unknown,
+) {
+  if (asyncJobConfig.argumentValidator === undefined) {
+    return null;
+  }
+
+  const { success, data, error } =
+    asyncJobConfig.argumentValidator.safeParse(jobArguments);
+
+  if (!success) {
+    throw new InvalidInternalStateException(
+      `Invalid Async Job arguments for '${asyncJobConfig.name}' ` +
+        error.toString(),
+    );
+  }
+
+  return data;
+}
