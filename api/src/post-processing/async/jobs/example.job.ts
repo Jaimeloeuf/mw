dc793143f89845c1b63628bca8fdcd14e12deef7
Jaime <jaimeloeuf@gmail.com>
2024-12-16 01:24:03 +0800

    Add example AsyncJobType and a mapping.

---
+++ api/src/post-processing/async/jobs/example.job.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+import { logger } from "../../../logging/index.js";
+import { json } from "../../../utils/index.js";
+import { AsyncJobMachineType } from "../AsyncJobMachineType.js";
+import { AsyncJobPriority } from "../AsyncJobPriority.js";
+import { defineAsyncJobType } from "../defineAsyncJobType.js";
+
+/**
+ * Example Job for reference and demo.
+ */
+export default defineAsyncJobType({
+  id: "186e39bb-0bd5-451c-8a90-557ae9a54ca8",
+  name: "ExampleJob",
+  machineType: AsyncJobMachineType.web,
+  priority: AsyncJobPriority.p1,
+  argumentValidator: z.object({
+    test: z.string(),
+  }),
+  async run(args) {
+    logger.verbose(
+      this.name,
+      `Doing some compute in async tier, received args: ${json.stringifySafely(args)}`,
+    );
+
+    return {
+      success: true,
+      details: "success",
+    };
+  },
+});
---
+++ api/src/post-processing/async/mappingOfAsyncJobs.ts
@@ -0,0 +1,17 @@
+import type { AsyncJobType } from "./AsyncJobType.js";
+
+/**
+ * Mapping of `AsyncJobType['ID']` to `AsyncJobType`.
+ *
+ * Use `import` function to asynchronously load the job definition module, so
+ * that they are not all loaded on startup and only lazily loaded whenever they
+ * are used.
+ *
+ * @todo Codegen this instead of manually defining
+ */
+export const mappingOfAsyncJobs: Record<
+  string,
+  () => Promise<{ default: AsyncJobType }>
+> = {
+  "186e39bb-0bd5-451c-8a90-557ae9a54ca8": () => import("./jobs/example.job.js"),
+};
