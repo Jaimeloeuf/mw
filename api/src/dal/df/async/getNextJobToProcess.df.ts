da55fe64cb7ad9b1c17495e23acc7becb24d317c
Jaime <jaimeloeuf@gmail.com>
2024-12-15 08:20:16 +0800

    Allow user to specify machine type filter.

--- api/src/dal/df/async/getNextJobToProcess.df.ts
+++ api/src/dal/df/async/getNextJobToProcess.df.ts
@@ -9,53 +9,53 @@ import { dataFn } from "../dataFn.js";
  * DataFn to get a job from DB after marking it as pre-processing in a single
  * transaction.
  */
-export default dataFn(
-  async function asyncGetNextJobToProcess(): Promise<null | AsyncJob> {
-    const asyncJob = await apiDB.transaction().execute(async (db) => {
-      const asyncJob = await db
-        .selectFrom("async_job")
-        .selectAll()
-        .where("machine_type", "=", AsyncJobMachineType.web)
-        .orderBy("priority", "asc")
-        .limit(1)
-        .executeTakeFirst();
+export default dataFn(async function asyncGetNextJobToProcess(
+  machineType: AsyncJobMachineType,
+): Promise<null | AsyncJob> {
+  const asyncJob = await apiDB.transaction().execute(async (db) => {
+    const asyncJob = await db
+      .selectFrom("async_job")
+      .selectAll()
+      .where("machine_type", "=", machineType)
+      .orderBy("priority", "asc")
+      .limit(1)
+      .executeTakeFirst();
 
-      if (asyncJob === undefined) {
-        return null;
-      }
-
-      return await db
-        .updateTable("async_job")
-        .where("id", "=", asyncJob.id)
-        .set({
-          status: AsyncJobStatus.preProcessing,
-        })
-        .returningAll()
-        .executeTakeFirstOrThrow();
-    });
-
-    if (asyncJob === null) {
+    if (asyncJob === undefined) {
       return null;
     }
 
-    return {
-      id: asyncJob.id,
-      jobTypeID: asyncJob.job_type_id,
-      status: asyncJob.status,
-      priority: asyncJob.priority,
-      machineType: asyncJob.machine_type,
-      caller: asyncJob.caller,
-      stackTrace: asyncJob.stack_trace,
-      timeout: asyncJob.timeout,
-      timeScheduled: asyncJob.time_scheduled.toISOString(),
-      timeStart: asyncJob.time_start?.toISOString() ?? null,
-      timeFinish: asyncJob.time_finish?.toISOString() ?? null,
-      jobArguments:
-        asyncJob.job_arguments === null
-          ? null
-          : JSON.parse(asyncJob.job_arguments),
-      jobResult:
-        asyncJob.job_result === null ? null : JSON.parse(asyncJob.job_result),
-    };
-  },
-);
+    return await db
+      .updateTable("async_job")
+      .where("id", "=", asyncJob.id)
+      .set({
+        status: AsyncJobStatus.preProcessing,
+      })
+      .returningAll()
+      .executeTakeFirstOrThrow();
+  });
+
+  if (asyncJob === null) {
+    return null;
+  }
+
+  return {
+    id: asyncJob.id,
+    jobTypeID: asyncJob.job_type_id,
+    status: asyncJob.status,
+    priority: asyncJob.priority,
+    machineType: asyncJob.machine_type,
+    caller: asyncJob.caller,
+    stackTrace: asyncJob.stack_trace,
+    timeout: asyncJob.timeout,
+    timeScheduled: asyncJob.time_scheduled.toISOString(),
+    timeStart: asyncJob.time_start?.toISOString() ?? null,
+    timeFinish: asyncJob.time_finish?.toISOString() ?? null,
+    jobArguments:
+      asyncJob.job_arguments === null
+        ? null
+        : JSON.parse(asyncJob.job_arguments),
+    jobResult:
+      asyncJob.job_result === null ? null : JSON.parse(asyncJob.job_result),
+  };
+});
