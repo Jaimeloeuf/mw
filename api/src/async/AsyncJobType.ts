cae62dbe7f865c36fd0419d12da718558cf5075c
Jaime <jaimeloeuf@gmail.com>
2025-01-04 00:00:51 +0800

    Use better Type name.

--- api/src/async/AsyncJobType.ts
+++ api/src/async/AsyncJobType.ts
@@ -6,38 +6,39 @@ import type { AsyncJobTypeConfig } from "./AsyncJobTypeConfig.js";
  * An object type to describe an Async Job Type, alongside a `scheduleJob`
  * method for users to schedule a Job of that Job Type.
  */
-export type AsyncJobType<T = any> = AsyncJobTypeConfig<T> & {
-  scheduleJob: (runOptions?: {
-    /**
-     * A simple string name to identify the caller that scheduled this job. If
-     * not set, it will default to the `<unspecified>` caller.
-     */
-    caller?: AsyncJob["caller"];
+export type AsyncJobType<AsyncJobArgumentType = any> =
+  AsyncJobTypeConfig<AsyncJobArgumentType> & {
+    scheduleJob: (runOptions?: {
+      /**
+       * A simple string name to identify the caller that scheduled this job. If
+       * not set, it will default to the `<unspecified>` caller.
+       */
+      caller?: AsyncJob["caller"];
 
-    /**
-     * Arguments for this Job.
-     */
-    jobArguments: T;
+      /**
+       * Arguments for this Job.
+       */
+      jobArguments: AsyncJobArgumentType;
 
-    /**
-     * Set a ISO DateTime string, so that the async job will only run AFTER this
-     * date and never before it.
-     */
-    startAfter?: AsyncJob["timeStartAfter"];
+      /**
+       * Set a ISO DateTime string, so that the async job will only run AFTER this
+       * date and never before it.
+       */
+      startAfter?: AsyncJob["timeStartAfter"];
 
-    /**
-     * Override the Async Job Type's default priority level.
-     */
-    priorityOverride?: AsyncJobTypeConfig["priority"];
+      /**
+       * Override the Async Job Type's default priority level.
+       */
+      priorityOverride?: AsyncJobTypeConfig["priority"];
 
-    /**
-     * Override the Async Job Type's default timeout.
-     */
-    timeoutOverride?: AsyncJobTypeConfig["timeout"];
+      /**
+       * Override the Async Job Type's default timeout.
+       */
+      timeoutOverride?: AsyncJobTypeConfig["timeout"];
 
-    /**
-     * Override the Async Job Type's default machine type.
-     */
-    machineTypeOverride?: AsyncJobMachineType;
-  }) => Promise<void>;
-};
+      /**
+       * Override the Async Job Type's default machine type.
+       */
+      machineTypeOverride?: AsyncJobMachineType;
+    }) => Promise<void>;
+  };
