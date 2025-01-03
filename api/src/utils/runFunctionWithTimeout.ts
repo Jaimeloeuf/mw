46abc8699a9f4792d3622060c86bb67dfb94db25
Jaime <jaimeloeuf@gmail.com>
2025-01-03 22:17:31 +0800

    Re-implement runFunctionWithTimeout:     Try with Promise.race, although it works, it doesnt really throw     and kill the original promise execution, which kind of defeats the     point of this.

--- api/src/utils/runFunctionWithTimeout.ts
+++ api/src/utils/runFunctionWithTimeout.ts
@@ -1,17 +1,5 @@
 import { TimeoutException } from "../exceptions/index.js";
 
-async function delayAndThrow(
-  timeoutInMilliseconds: number,
-  timeoutExceptionMessage: string,
-  controller: { skip: boolean },
-) {
-  await new Promise((res) => setTimeout(res, timeoutInMilliseconds));
-
-  if (!controller.skip) {
-    throw new TimeoutException(timeoutExceptionMessage);
-  }
-}
-
 /**
  * Runs a given function with a timeout, and will stop the given function's
  * execution once the timeout has been hit, by having a `TimeoutException`
@@ -28,19 +16,13 @@ export async function runFunctionWithTimeout<T extends (...args: any) => any>(
   timeoutInMilliseconds: number,
   timeoutExceptionMessage: string,
 ) {
-  const controller = { skip: false };
-
-  try {
-    delayAndThrow(timeoutInMilliseconds, timeoutExceptionMessage, controller);
-
-    const result = await fn();
-
-    controller.skip = true;
-
-    return result;
-  } catch (error) {
-    // Always clear timeout before re-throwing error / bubbling error up.
-    controller.skip = true;
-    throw error;
-  }
+  return Promise.race([
+    fn(),
+    new Promise((_, reject) =>
+      setTimeout(
+        () => reject(new TimeoutException(timeoutExceptionMessage)),
+        timeoutInMilliseconds,
+      ),
+    ),
+  ]);
 }
