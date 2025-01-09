580e3149eb36782be094f828fb9dc8fee2133afb
Jaime <jaimeloeuf@gmail.com>
2025-01-09 21:40:17 +0800

    WIP experiment add Orm BaseEnt.

---
+++ api/src/dal/orm/Orm.ts
@@ -0,0 +1,27 @@
+import { UnimplementedException } from "../../exceptions/index.js";
+
+/**
+ * Base abstract Entity class for all Ents to extend/implement
+ */
+export abstract class BaseEnt {
+  /**
+   * The Ent's data
+   */
+  abstract data: Record<string, unknown>;
+
+  /**
+   * Implement this to provide a consistent way for sending the data over the
+   * network using JSON. This also means that all data in an Ent should use
+   * primitive types that are JSON serializable.
+   */
+  abstract jsonSerialise(): string;
+
+  /**
+   * Recommended to use `zod` instead of parsing with `JSON.parse`.
+   */
+  static jsonParse(jsonString: string): unknown {
+    throw new UnimplementedException(
+      `jsonParse not implemented! Cannot parse: ${jsonString}`,
+    );
+  }
+}
