991bd1fc6ed8e4bf8fab13cd7d17ae50056807e2
Jaime <jaimeloeuf@gmail.com>
2025-01-15 23:01:52 +0800

    Add default ent fields to migration template.

--- api/src/dal/kysely/migration-cli/createKyselyMigrationFile/migrationFileTemplate.ts
+++ api/src/dal/kysely/migration-cli/createKyselyMigrationFile/migrationFileTemplate.ts
@@ -5,7 +5,13 @@ import { sql } from "kysely";
 const tableName = "your_table_name";
 
 export async function up(db: Kysely<any>): Promise<void> {
-  await db.schema.createTable(tableName);
+  await db.schema
+    .createTable(tableName)
+    .addColumn("id", "text", (col) => col.primaryKey())
+    .addColumn("created_at", "timestamp", (col) => col.notNull())
+    .addColumn("updated_at", "timestamp", (col) => col.notNull())
+    /* Add whatever other columns you need */
+    .execute();
 }
 
 export async function down(db: Kysely<any>): Promise<void> {
