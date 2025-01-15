5bd7f501bfc7edeb7059798343e47ea588a9f302
Jaime <jaimeloeuf@gmail.com>
2025-01-15 23:06:46 +0800

    Add default ent field to blog suscriber table.

--- api/src/dal/kysely/migrations/20240718_1_create_blog_subscriber_table.ts
+++ api/src/dal/kysely/migrations/20240718_1_create_blog_subscriber_table.ts
@@ -1,16 +1,13 @@
 import type { Kysely } from "kysely";
 
-import { sql } from "kysely";
-
 const tableName = "blog_subscriber";
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
     .createTable(tableName)
     .addColumn("id", "text", (col) => col.primaryKey())
-    .addColumn("created_at", "timestamp", (col) =>
-      col.notNull().defaultTo(sql`now()`),
-    )
+    .addColumn("created_at", "timestamp", (col) => col.notNull())
+    .addColumn("updated_at", "timestamp", (col) => col.notNull())
     .addColumn("email", "text", (col) => col.notNull().unique())
     .execute();
 }
