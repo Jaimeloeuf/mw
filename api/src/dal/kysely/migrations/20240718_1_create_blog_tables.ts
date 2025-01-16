ee37204d6b4cefa598f5e2583a184c9b13e9a2b9
Jaime <jaimeloeuf@gmail.com>
2025-01-16 23:14:41 +0800

    Expand blog migration script:     For both Blog and BlogSubscriber tables.

---
+++ api/src/dal/kysely/migrations/20240718_1_create_blog_tables.ts
@@ -0,0 +1,28 @@
+import type { Kysely } from "kysely";
+
+const blogTableName = "blog";
+const blogSubscriberTableName = "blog_subscriber";
+
+export async function up(db: Kysely<any>): Promise<void> {
+  await db.schema
+    .createTable(blogTableName)
+    .addColumn("id", "text", (col) => col.primaryKey())
+    .addColumn("created_at", "timestamp", (col) => col.notNull())
+    .addColumn("updated_at", "timestamp", (col) => col.notNull())
+    .addColumn("name", "text", (col) => col.notNull())
+    .addColumn("owner_email", "text", (col) => col.notNull())
+    .execute();
+
+  await db.schema
+    .createTable(blogSubscriberTableName)
+    .addColumn("id", "text", (col) => col.primaryKey())
+    .addColumn("created_at", "timestamp", (col) => col.notNull())
+    .addColumn("updated_at", "timestamp", (col) => col.notNull())
+    .addColumn("email", "text", (col) => col.notNull().unique())
+    .execute();
+}
+
+export async function down(db: Kysely<any>): Promise<void> {
+  await db.schema.dropTable(blogSubscriberTableName).execute();
+  await db.schema.dropTable(blogTableName).execute();
+}
