8e458b9f5c211d604d6c9d6fa0f774e2f52f16a7
Jaime <jaimeloeuf@gmail.com>
2025-01-21 00:22:56 +0800

    Add mutation to CreateNew Johari to call API.

--- spas/johari/src/components/CreateNew.tsx
+++ spas/johari/src/components/CreateNew.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 import { useEmptyJohariOptionsSet } from "./useEmptyJohariOptionsSet";
 import Answer from "./Answer";
 
@@ -8,13 +10,35 @@ import Answer from "./Answer";
 export default function () {
   const [name, setName] = useState("");
   const johariAnswerOptions = useEmptyJohariOptionsSet();
+  const navigate = useNavigate();
 
-  function onSubmit() {
-    if (name === "") {
-      alert("Name cannot be empty!");
-      return;
-    }
-  }
+  const mutation = useMutation({
+    mutationFn() {
+      if (name === "") {
+        throw new Error("Name cannot be empty!");
+      }
+
+      return fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/api/v1/johari/create`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            words: johariAnswerOptions.selectedWords,
+          }),
+        }
+      ).then((res) => res.json());
+    },
+    onSuccess(data) {
+      // @todo Generate a shareable link
+      // @todo Show button to let them redirect to their main display page
+      const johariID = data.data;
+      navigate(`/view/${johariID}`);
+    },
+  });
 
   return (
     <div>
@@ -36,7 +60,7 @@ export default function () {
       </div>
 
       <div className="pt-8">
-        <Answer {...johariAnswerOptions} onSubmit={onSubmit} />
+        <Answer {...johariAnswerOptions} onSubmit={mutation.mutate} />
       </div>
     </div>
   );
