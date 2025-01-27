8d197b6fb69f81df99e8e7f8e14061b48344de3c
Jaime <jaimeloeuf@gmail.com>
2025-01-28 07:31:53 +0800

    Add default global error boundary.

--- spas/johari/src/components/App.tsx
+++ spas/johari/src/components/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router";
+import { ErrorBoundary } from "react-error-boundary";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../queryClient.ts";
 import Home from "./Home.tsx";
@@ -12,23 +13,31 @@ import View from "./View.tsx";
  */
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <div className="flex flex-row justify-center">
-          <div className="p-8 max-w-screen-sm w-full">
-            <Link to="/">
-              <p className="text-4xl pb-8">Johari</p>
-            </Link>
-
-            <Routes>
-              <Route index element={<Home />} />
-              <Route path="create" element={<CreateNew />} />
-              <Route path="answer/:johariID" element={<AnswerForOthers />} />
-              <Route path="view/:johariID" element={<View />} />
-            </Routes>
-          </div>
+    <ErrorBoundary
+      fallback={
+        <div className="w-dvw h-dvh flex flex-row justify-center items-center">
+          Oops... something went seriously wrong...
         </div>
-      </BrowserRouter>
-    </QueryClientProvider>
+      }
+    >
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <div className="flex flex-row justify-center">
+            <div className="p-8 max-w-screen-sm w-full">
+              <Link to="/">
+                <p className="text-4xl pb-8">Johari</p>
+              </Link>
+
+              <Routes>
+                <Route index element={<Home />} />
+                <Route path="create" element={<CreateNew />} />
+                <Route path="answer/:johariID" element={<AnswerForOthers />} />
+                <Route path="view/:johariID" element={<View />} />
+              </Routes>
+            </div>
+          </div>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
