e067444acf9651f5361b52ac358c5d2573da81cb
Jaime <jaimeloeuf@gmail.com>
2025-02-02 00:29:28 +0800

    Show card after answering instead of auto redirect.

--- spas/johari/src/components/AnswerForOthers.tsx
+++ spas/johari/src/components/AnswerForOthers.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { useParams, useNavigate } from "react-router";
+import { useParams, Link } from "react-router";
 import { useMutation } from "@tanstack/react-query";
 import { useEmptyJohariOptionsSet } from "./useEmptyJohariOptionsSet";
 import Answer from "./Answer";
 import { useJohari } from "./useJohari";
 import ErrorModal from "./ErrorModal";
+import type { johariCreateJohariAnswerController_OutputFullDTO } from "@generated-api-types";
 
 export default function () {
   const { johariID } = useParams();
@@ -17,30 +18,26 @@ export default function () {
 
   const [name, setName] = useState("");
   const johariAnswerOptions = useEmptyJohariOptionsSet();
-  const navigate = useNavigate();
 
-  const mutation = useMutation({
-    mutationFn() {
-      return fetch(
-        `${import.meta.env.VITE_API_BASE_URL}/api/v1/johari/answer`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            johariID,
-            name,
-            words: johariAnswerOptions.selectedWords,
-          }),
-        }
-      ).then((res) => res.json());
-    },
-    onSuccess() {
-      // @todo Show button to let them redirect to their main display page or Create their own one
-      navigate(`/view/${johariID}`);
-    },
-  });
+  const mutation =
+    useMutation<johariCreateJohariAnswerController_OutputFullDTO>({
+      mutationFn() {
+        return fetch(
+          `${import.meta.env.VITE_API_BASE_URL}/api/v1/johari/answer`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              johariID,
+              name,
+              words: johariAnswerOptions.selectedWords,
+            }),
+          }
+        ).then((res) => res.json());
+      },
+    });
 
   if (status === "pending") {
     return (
@@ -59,6 +56,64 @@ export default function () {
 
   if (mutation.isError) {
     return <ErrorModal error={mutation.error} clearError={mutation.reset} />;
+  }
+
+  if (mutation.isSuccess) {
+    return (
+      <div className="flex flex-row items-center">
+        <div className="w-full">
+          <p className="py-10 text-3xl font-thin">Thank you for your answer!</p>
+
+          <div className="border border-green-600 rounded-lg">
+            <Link to="/create" className="text-green-600 text-2xl font-light">
+              <div className="p-4 flex w-full flex-row items-center justify-between">
+                Create your Johari Window
+                <svg
+                  className="h-5 w-5 shrink-0 rotate-90"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 5 5 1 1 5"
+                  />
+                </svg>
+              </div>
+            </Link>
+          </div>
+
+          <div className="py-8 italic font-thin text-3xl text-center">OR</div>
+
+          <div className="border border-zinc-300 rounded-lg">
+            <Link to={`/view/${johariID}`} className="text-zinc-600">
+              <div className="p-4 flex w-full flex-row items-center justify-between">
+                See {data.data.name}'s Johari Window
+                <svg
+                  className="h-3 w-3 shrink-0 rotate-90"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 5 5 1 1 5"
+                  />
+                </svg>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
