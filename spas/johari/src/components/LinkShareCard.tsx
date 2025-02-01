5c33070f9c222e0245375bcfb8a10746b3fcf457
Jaime <jaimeloeuf@gmail.com>
2025-02-02 00:08:03 +0800

    Refactor UI.

---
+++ spas/johari/src/components/LinkShareCard.tsx
@@ -0,0 +1,36 @@
+import { getLink } from "./getLink";
+import CopyOnClick from "./CopyOnClick";
+
+export default function NoAnswerCard(props: {
+  johariID: string;
+  ownerName: string;
+}) {
+  const answerLink = getLink.toAnswerJohari(props.johariID);
+  return (
+    <div className="p-6 border border-zinc-200 rounded-lg">
+      <p className="pb-4 font-light">
+        Copy or Share the link and get your family, friends, colleagues, etc...
+        to answer for you!
+      </p>
+      <div className="flex flex-row justify-between gap-8">
+        <CopyOnClick textToCopy={answerLink}>
+          <button className="px-4 py-2 w-full border border-zinc-200 rounded-lg">
+            Copy link
+          </button>
+        </CopyOnClick>
+        <button
+          className="px-4 py-2 w-full border border-zinc-200 rounded-lg"
+          onClick={() =>
+            navigator.share({
+              title: `Johari Window for ${props.ownerName}`,
+              text: `Help ${props.ownerName} better understand their personality!`,
+              url: answerLink,
+            })
+          }
+        >
+          Share link
+        </button>
+      </div>
+    </div>
+  );
+}
--- spas/johari/src/components/NoAnswerCard.tsx
+++ spas/johari/src/components/NoAnswerCard.tsx
@@ -1,40 +1,12 @@
-import { getLink } from "./getLink";
-import CopyOnClick from "./CopyOnClick";
+import LinkShareCard from "./LinkShareCard";
 
-export default function NoAnswerCard(props: {
-  johariID: string;
-  ownerName: string;
-}) {
-  const answerLink = getLink.toAnswerJohari(props.johariID);
+export default function NoAnswerCard(
+  ...props: Parameters<typeof LinkShareCard>
+) {
   return (
     <div>
       <p className="py-8 text-4xl font-thin italic">No answers yet</p>
-
-      <div className="p-6 border border-zinc-200 rounded-lg">
-        <p className="pb-4 font-light">
-          Copy or Share the link and get your family, friends, colleagues,
-          etc... to answer for you!
-        </p>
-        <div className="flex flex-row justify-between gap-8">
-          <CopyOnClick textToCopy={answerLink}>
-            <button className="px-4 py-2 w-full border border-zinc-200 rounded-lg">
-              Copy link
-            </button>
-          </CopyOnClick>
-          <button
-            className="px-4 py-2 w-full border border-zinc-200 rounded-lg"
-            onClick={() =>
-              navigator.share({
-                title: `Johari Window for ${props.ownerName}`,
-                text: `Help ${props.ownerName} better understand their personality!`,
-                url: answerLink,
-              })
-            }
-          >
-            Share link
-          </button>
-        </div>
-      </div>
+      <LinkShareCard {...props[0]} />
     </div>
   );
 }
