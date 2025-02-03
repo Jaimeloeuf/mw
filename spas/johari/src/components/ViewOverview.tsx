f9731653a6c05e24b7e4046507ae4cce755986a2
Jaime <jaimeloeuf@gmail.com>
2025-02-04 00:29:22 +0800

    Refactor out component.

--- spas/johari/src/components/ViewOverview.tsx
+++ spas/johari/src/components/ViewOverview.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
 import { useJohariOptions } from "./useJohariOptions";
 import { useJohariAnswerers } from "./useJohariAnswerers";
 import { useJohariWindow } from "./useJohariWindow";
+import { WordWithTooltip } from "./WordWithTooltip";
 
 /**
  * Component that the Johari window for users to see the high level overview.
@@ -72,35 +72,5 @@ export default function (props: {
         </div>
       </div>
     </div>
-  );
-}
-
-function WordWithTooltip(props: { word: string; ppl: Array<string> }) {
-  if (props.ppl.length === 0) {
-    return <p className="font-extralight text-sm">{props.word}</p>;
-  }
-
-  const [isHover, setIsHover] = useState(false);
-
-  return (
-    <>
-      <p
-        onMouseEnter={() => setIsHover(true)}
-        onMouseLeave={() => setIsHover(false)}
-        className="cursor-default font-light text-sm sm:text-base"
-      >
-        ({props.ppl.length}) {props.word}
-      </p>
-
-      {isHover && (
-        <div
-          role="tooltip"
-          className="absolute z-10 inline-block pl-2 pr-4 py-1 transition-opacity border border-zinc-400 bg-zinc-50 rounded-lg shadow-2xl"
-        >
-          {props.word}: {props.ppl.join(", ")}
-          <div className="tooltip-arrow" data-popper-arrow></div>
-        </div>
-      )}
-    </>
   );
 }
