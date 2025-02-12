2db2fa20eb97fb66f480c0b1f7b0311f940441de
Jaime <jaimeloeuf@gmail.com>
2025-02-12 20:04:48 +0800

    Delete enum type from async_job table: 1.  Delete enum types. 2.  Convert use of enum types to use string/text fields instead, since     enums arent as extensible as free form string/text, and if     performance is truly an issue it could be mitigated with an index.

--- api/src/dal/kysely/migrations/20241208_1_create_async_job_table.ts
+++ api/src/dal/kysely/migrations/20241208_1_create_async_job_table.ts
@@ -1,38 +1,14 @@
 import type { Kysely } from "kysely";
 
-import { sql } from "kysely";
-
-const asyncJobStatusEnumName = "async_job_status";
-const asyncJobMachineTypeEnumName = "async_job_machine_type";
 const asyncJobTableName = "async_job";
 
 export async function up(db: Kysely<any>): Promise<void> {
   await db.schema
-    .createType(asyncJobStatusEnumName)
-    .asEnum([
-      "queued",
-      "pre-processing",
-      "started",
-      "finish-success",
-      "finish-fail",
-    ])
-    .execute();
-
-  await db.schema
-    .createType(asyncJobMachineTypeEnumName)
-    .asEnum(["web"])
-    .execute();
-
-  await db.schema
     .createTable(asyncJobTableName)
     .addColumn("id", "text", (col) => col.primaryKey())
     .addColumn("job_type_id", "text", (col) => col.notNull())
-    .addColumn("status", sql.raw(asyncJobStatusEnumName), (col) =>
-      col.notNull(),
-    )
-    .addColumn("machine_type", sql.raw(asyncJobMachineTypeEnumName), (col) =>
-      col.notNull(),
-    )
+    .addColumn("status", "text", (col) => col.notNull())
+    .addColumn("machine_type", "text", (col) => col.notNull())
     .addColumn("priority", "int2", (col) => col.notNull())
     .addColumn("caller", "text", (col) => col.notNull())
     .addColumn("stack_trace", "text", (col) => col.notNull())
@@ -49,8 +25,4 @@ export async function up(db: Kysely<any>): Promise<void> {
 
 export async function down(db: Kysely<any>): Promise<void> {
   await db.schema.dropTable(asyncJobTableName).execute();
-
-  // Delete the enum types
-  await db.schema.dropType(asyncJobMachineTypeEnumName).execute();
-  await db.schema.dropType(asyncJobStatusEnumName).execute();
 }
