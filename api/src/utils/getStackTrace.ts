b751b7063e563387d6715f42a29019fd972c8964
Jaime <jaimeloeuf@gmail.com>
2025-02-15 00:39:14 +0800

    Add docs for v8 specific details in getStackTrace.

--- api/src/utils/getStackTrace.ts
+++ api/src/utils/getStackTrace.ts
@@ -16,12 +16,21 @@ export const getStackTrace = {
         // The first line is the word 'Error' and isnt part of the stack frames.
         // The second line is the stack frame for this getStackTrace method,
         // which should not be part of the returned stack trace to users.
+        //
+        // Note that this is specific to v8 engine, for any other engine it does
+        // not start with the word `Error` so it should only drop a single line.
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/stack
+        //
+        // Alternatively if `Error.captureStackTrace` becomes standard, it could
+        // be a better choice.
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error/captureStackTrace
         .slice(2 + numberOfStackFramesToDrop)
         .map((s) =>
           s
             // Remove the tab indentation
             .trim()
             // Remove 'at ' at the start to only keep symbol name and location
+            // This is string format is also specific to v8 engine
             .replace("at ", ""),
         ) ?? []
     );
