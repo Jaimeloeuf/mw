b3e98424a94354a0de08e719e70ca92ee86280b3
Jaime <jaimeloeuf@gmail.com>
2025-02-19 00:13:00 +0800

    Add $ResultTuple to global scope.

--- api/src/types/utils/GlobalUtilityTypes.ts
+++ api/src/types/utils/GlobalUtilityTypes.ts
@@ -1,3 +1,5 @@
+import type { Exception } from "../../exceptions/Exception.js";
+
 /**
  * Declaring all globally available utility types here with a $ prefix to
  * signify that these are in the global scope.
@@ -19,4 +21,23 @@ declare global {
    * - `[T, T, T]` is valid
    */
   type $NonEmptyArray<T> = [T, ...T[]];
+
+  /**
+   * Result tuple type encapsulates a result that could either be successful or
+   * not successful.
+   *
+   * Where it is successful, its type will be `TResult`, while if unsuccessful,
+   * its types will be `TException`.
+   *
+   * Exception is always positioned first in the tuple so users are forced to
+   * either handle it, or ignore it explicitly with something like `_`.
+   *
+   * `TException` extends `Exception` instead of `Error` to indicate that only
+   * recoverable issues, i.e. exceptions not errors, should be allowed to wrap
+   * in a `$ResultTuple`, while an Error (irrecoverable state) should always be
+   * thrown instead.
+   */
+  type $ResultTuple<TResult, TException extends Exception = Exception> =
+    | [null, TResult]
+    | [TException | null];
 }
