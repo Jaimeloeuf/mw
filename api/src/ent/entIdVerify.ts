b8e10bbeb81155a6842996619ad8a599cd9ad350
Jaime <jaimeloeuf@gmail.com>
2025-04-28 23:12:55 +0800

    Fix entIdVerify ID length verification:     Fix bug where the ID length wasnt actually being verified, where it     only verified if the postfix started at the correct position.

--- api/src/ent/entIdVerify.ts
+++ api/src/ent/entIdVerify.ts
@@ -3,13 +3,15 @@ import type { EntClass } from "./EntClass.js";
 
 /**
  * Verify if the Ent ID is valid for a given Ent Type / Class.
+ *
+ * Checks:
+ * 1. If the entID is exactly 41 characters long, length of UUID + postfix
+ * 1. If the entID ends with the correct postfix of "_" and the entTypeID
  */
 export function entIdVerify(
   entClass: typeof BaseEnt | EntClass<BaseEnt>,
   entID: string,
 ): boolean {
   const entTypeID = (entClass as unknown as typeof BaseEnt).EntTypeID;
-  // By hardcoding 41, we can also ensure that the ID is of the right length,
-  // and not just an arbitrary length string that happens to end with EntTypeID.
-  return entID.endsWith(`_${entTypeID}`, 41);
+  return entID.length === 41 && entID.endsWith(`_${entTypeID}`);
 }
