3bc23cb6fb3c2d8699612ea67d3f1b7a72e89d8f
Jaime <jaimeloeuf@gmail.com>
2025-05-14 23:58:52 +0800

    Refactor runFnSafely to use $ResultTuple.

--- api/src/global/utils/runFnSafely.ts
+++ api/src/global/utils/runFnSafely.ts
@@ -1,39 +1,24 @@
 declare global {
   /**
-   * Utility wrapper function to wrap a function sync to prevent it from throwing.
+   * Utility function run a synchronous function safely to prevent it from
+   * throwing and returns a `$ResultTuple`.
    *
-   * **If you need to wrap an async function, use `$runAsyncFnSafely`**
-   * **If you need to wrap a Promise, use `$awaitPromiseSafely`**
-   *
-   * ## Return Type
-   * This will always return a 2 element tuple, whose type is
-   * `[err, null] | [null, result]`. Similar to Go error handling, where if there
-   * is an error the first element will be set to it and the second element will
-   * be null, and vice versa if no errors thrown.
-   *
-   * You can then use TS type narrowing to determine if the function threw an
-   * error or not. For example
-   * ```typescript
-   * export const [err, result] = $runFnSafely(() => ...);
-   * if (err !== null){
-   *     console.error('Operation failed with', err);
-   *     return;
-   * }
-   * result; // This will type narrow to ResultType
-   * ```
+   * ### Alternatively
+   * 1. Run an async function with global util `$runAsyncFnSafely`.
+   * 1. Await a Promise with global util `$awaitPromiseSafely`.
    */
   function $runFnSafely<
     T extends (...args: any) => any,
     FnArgs extends Parameters<T>,
     SuccessfulReturnType extends ReturnType<T> = ReturnType<T>,
-  >(fn: T, ...args: FnArgs): [null, SuccessfulReturnType] | [Error, null];
+  >(fn: T, ...args: FnArgs): $ResultTuple<SuccessfulReturnType, Error>;
 }
 
 globalThis.$runFnSafely = function $runFnSafely<
   T extends (...args: any) => any,
   FnArgs extends Parameters<T>,
   SuccessfulReturnType extends ReturnType<T> = ReturnType<T>,
->(fn: T, ...args: FnArgs): [null, SuccessfulReturnType] | [Error, null] {
+>(fn: T, ...args: FnArgs): $ResultTuple<SuccessfulReturnType, Error> {
   try {
     return [null, fn(...args)];
   } catch (e) {
