8421cc59a94b6c72570ceb7a534b2bb47a6f0607
Jaime <jaimeloeuf@gmail.com>
2025-08-22 00:17:55 +0800

    Add new EntSchema setup and validation error type.

---
+++ api/src/entschema/lib/EntSchemaSetupAndValidationError.ts
@@ -0,0 +1,5 @@
+/**
+ * Error used specifically to identify issues from EntSchema setup and
+ * validation process.
+ */
+export class EntSchemaSetupAndValidationError extends Error {}
--- api/src/entschema/lib/index.ts
+++ api/src/entschema/lib/index.ts
@@ -6,4 +6,5 @@ export * from "./EntEdgeDefinition.js";
 export * from "./EntFieldDefinition.js";
 export * from "./EntSchema.js";
 export * from "./EntSchemaConstructor.js";
+export * from "./EntSchemaSetupAndValidationError.js";
 export * from "./EntSchemaValidatedData.js";
