242bd86b3adf99d579bb2f3a47aace9ad76b9b48
Jaime <jaimeloeuf@gmail.com>
2025-08-23 16:27:48 +0800

    Cache EntSchema validation and setup call.

--- api/src/entschema/lib/EntSchema.ts
+++ api/src/entschema/lib/EntSchema.ts
@@ -3,18 +3,27 @@
 import type { EntEdgeDefinition } from "./EntEdgeDefinition.js";
 import type { EntFieldDefinition } from "./EntFieldDefinition.js";
 import type { EntSchemaClass } from "./EntSchemaClass.js";
+import type { EntSchemaValidatedData } from "./EntSchemaValidatedData.js";
 
 import { EntSchemaValidateAndSetup } from "./EntSchemaValidateAndSetup.js";
 
 export abstract class EntSchema {
+  static entSchemaValidatedData: EntSchemaValidatedData | null = null;
+
   /**
-   * Validate the current schema and set it up to use the validated data object.
+   * Validate the current schema and set it up to use the validated data object,
+   * note that this data will be cached after first use.
    */
-  static validateAndSetup() {
-    // @ts-expect-error since `EntSchema` is an abstract class, the only way
-    // this static method can be invoked is via a concrete class, so it can be
-    // safely assumed that it will be a concrete class here
-    return EntSchemaValidateAndSetup(this as EntSchemaClass);
+  static validateAndSetup(): EntSchemaValidatedData {
+    if (this.entSchemaValidatedData === null) {
+      this.entSchemaValidatedData = EntSchemaValidateAndSetup(
+        // @ts-expect-error since `EntSchema` is an abstract class, the only way
+        // this static method can be invoked is via a concrete class, so it can
+        // be safely assumed that it will be a concrete class here
+        this as EntSchemaClass,
+      );
+    }
+    return this.entSchemaValidatedData;
   }
 
   abstract EntTypeID: string;
