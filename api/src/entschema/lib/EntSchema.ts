49cda6e8d5f257a1c3dd9a557a0632e663aeff5f
Jaime <jaimeloeuf@gmail.com>
2025-08-23 16:23:41 +0800

    Add static validateAndSetup method to EntSchema:     So that it can be called directly on the schema class itself instead     of importing and using the function.

--- api/src/entschema/lib/EntSchema.ts
+++ api/src/entschema/lib/EntSchema.ts
@@ -2,8 +2,21 @@
 
 import type { EntEdgeDefinition } from "./EntEdgeDefinition.js";
 import type { EntFieldDefinition } from "./EntFieldDefinition.js";
+import type { EntSchemaClass } from "./EntSchemaClass.js";
+
+import { EntSchemaValidateAndSetup } from "./EntSchemaValidateAndSetup.js";
 
 export abstract class EntSchema {
+  /**
+   * Validate the current schema and set it up to use the validated data object.
+   */
+  static validateAndSetup() {
+    // @ts-expect-error since `EntSchema` is an abstract class, the only way
+    // this static method can be invoked is via a concrete class, so it can be
+    // safely assumed that it will be a concrete class here
+    return EntSchemaValidateAndSetup(this as EntSchemaClass);
+  }
+
   abstract EntTypeID: string;
 
   // @todo To implement stricter typing or perhaps do runtime validation instead
