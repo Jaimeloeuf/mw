16db769785f01b9caf4603ab7fa8ae97ee454f75
Jaime <jaimeloeuf@gmail.com>
2025-08-24 22:12:20 +0800

    Export EntSchemaClass type.

--- api/src/entschema/lib/index.ts
+++ api/src/entschema/lib/index.ts
@@ -5,6 +5,7 @@
 export * from "./EntEdgeDefinition.js";
 export * from "./EntFieldDefinition.js";
 export * from "./EntSchema.js";
+export * from "./EntSchemaClass.js";
 export * from "./EntSchemaConstructor.js";
 export * from "./EntSchemaSetupAndValidationError.js";
 export * from "./EntSchemaValidateAndSetup.js";
