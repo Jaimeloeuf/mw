2d325f2c1721a7a11982f31e25927e71e3af4992
Jaime <jaimeloeuf@gmail.com>
2025-08-27 00:05:45 +0800

    Tmp replace EntJohariOperator with dynamic one.

--- api/src/ents/EntJohari/EntJohariOperators.ts
+++ api/src/ents/EntJohari/EntJohariOperators.ts
@@ -1,78 +1,83 @@
-import { apiDB } from "../../dal/kysely/index.js";
-import { defineEntOperators } from "../../ent/index.js";
-import { EntJohari } from "./EntJohari.js";
+// import { apiDB } from "../../dal/kysely/index.js";
+// import { defineEntOperators } from "../../ent/index.js";
+// import { EntJohari } from "./EntJohari.js";
 
-export const EntJohariOperators = defineEntOperators(EntJohari, {
-  CRUD: {
-    async create(ent) {
-      await apiDB
-        .insertInto("johari")
-        .values({
-          id: ent.data.id,
-          created_at: ent.data.createdAt,
-          updated_at: ent.data.updatedAt,
-          name: ent.data.name,
-          words: ent.data.words,
-        })
-        .returningAll()
-        .executeTakeFirstOrThrow();
-    },
+// export const EntJohariOperators = defineEntOperators(EntJohari, {
+//   CRUD: {
+//     async create(ent) {
+//       await apiDB
+//         .insertInto("johari")
+//         .values({
+//           id: ent.data.id,
+//           created_at: ent.data.createdAt,
+//           updated_at: ent.data.updatedAt,
+//           name: ent.data.name,
+//           words: ent.data.words,
+//         })
+//         .returningAll()
+//         .executeTakeFirstOrThrow();
+//     },
 
-    async get(id) {
-      const data = await apiDB
-        .selectFrom("johari")
-        .selectAll()
-        .where("id", "=", id)
-        .executeTakeFirstOrThrow();
+//     async get(id) {
+//       const data = await apiDB
+//         .selectFrom("johari")
+//         .selectAll()
+//         .where("id", "=", id)
+//         .executeTakeFirstOrThrow();
 
-      return new EntJohari({
-        id: data.id,
-        createdAt: data.created_at,
-        updatedAt: data.updated_at,
-        name: data.name,
-        words: data.words,
-      });
-    },
+//       return new EntJohari({
+//         id: data.id,
+//         createdAt: data.created_at,
+//         updatedAt: data.updated_at,
+//         name: data.name,
+//         words: data.words,
+//       });
+//     },
 
-    async getMany(ids) {
-      const data = await apiDB
-        .selectFrom("johari")
-        .selectAll()
-        .where("id", "in", ids)
-        .execute();
+//     async getMany(ids) {
+//       const data = await apiDB
+//         .selectFrom("johari")
+//         .selectAll()
+//         .where("id", "in", ids)
+//         .execute();
 
-      return data.map(
-        (data) =>
-          new EntJohari({
-            id: data.id,
-            createdAt: data.created_at,
-            updatedAt: data.updated_at,
-            name: data.name,
-            words: data.words,
-          }),
-      );
-    },
+//       return data.map(
+//         (data) =>
+//           new EntJohari({
+//             id: data.id,
+//             createdAt: data.created_at,
+//             updatedAt: data.updated_at,
+//             name: data.name,
+//             words: data.words,
+//           }),
+//       );
+//     },
 
-    async update(ent) {
-      await apiDB
-        .updateTable("johari")
-        .where("id", "=", ent.data.id)
-        .set({
-          updated_at: ent.data.updatedAt,
-          name: ent.data.name,
-          words: ent.data.words,
-        })
-        .execute();
-    },
+//     async update(ent) {
+//       await apiDB
+//         .updateTable("johari")
+//         .where("id", "=", ent.data.id)
+//         .set({
+//           updated_at: ent.data.updatedAt,
+//           name: ent.data.name,
+//           words: ent.data.words,
+//         })
+//         .execute();
+//     },
 
-    async delete(id) {
-      await apiDB
-        .deleteFrom("johari")
-        .where("id", "=", id)
-        .returningAll()
-        .executeTakeFirst();
-    },
-  },
+//     async delete(id) {
+//       await apiDB
+//         .deleteFrom("johari")
+//         .where("id", "=", id)
+//         .returningAll()
+//         .executeTakeFirst();
+//     },
+//   },
 
-  custom: {},
-});
+//   custom: {},
+// });
+
+import { createDynamicEntOperators } from "../../ent/createDynamicEntOperators.js";
+import { EntJohariSchema } from "../../entschema/EntJohariSchema.js";
+
+export const EntJohariOperators = createDynamicEntOperators(EntJohariSchema);
