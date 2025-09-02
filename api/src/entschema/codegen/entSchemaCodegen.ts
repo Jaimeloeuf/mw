1019fba3a1176dd7e6971d3aee30b8afbe0f3add
Jaime <jaimeloeuf@gmail.com>
2025-09-02 21:12:16 +0800

    WIP Add codegen for EntOperators.

--- api/src/entschema/codegen/entSchemaCodegen.ts
+++ api/src/entschema/codegen/entSchemaCodegen.ts
@@ -9,6 +9,7 @@ import type { EntSchemaValidatedData } from "../lib/index.js";
 
 import { logger } from "../../logging/index.js";
 import { generateEntFromEntSchema } from "./generateEntFromEntSchema.js";
+import { generateEntOperatorsFromEntSchema } from "./generateEntOperatorsFromEntSchema.js";
 
 async function entSchemaCodegen({
   entSchema,
@@ -52,6 +53,62 @@ async function entSchemaCodegen({
   logger.info(
     entSchemaCodegen.name,
     `Generated '${entClassName}' from '${entSchema.name}'`,
+  );
+}
+
+async function codegenCrudOperators({
+  entSchema,
+  entClassName,
+  entSchemaInstance,
+}: EntSchemaValidatedData) {
+  const generatedEntNode = generateEntOperatorsFromEntSchema(
+    entSchemaInstance,
+    {
+      entClassName,
+    },
+  );
+
+  const printer = ts.createPrinter({
+    newLine: ts.NewLineKind.LineFeed,
+    removeComments: false,
+  });
+
+  const generatedCode = printer.printNode(
+    ts.EmitHint.Unspecified,
+    generatedEntNode,
+    ts.createSourceFile(
+      "_.ts",
+      "",
+      ts.ScriptTarget.Latest,
+      false,
+      ts.ScriptKind.TS,
+    ),
+  );
+
+  const imports = `import { apiDB } from "../../dal/kysely/index.js";
+import { defineEntOperators } from "../../ent/index.js";
+import { EntBlog } from "./EntBlog.js";
+\n\n`;
+
+  const generatedCodeWithImports = imports + generatedCode;
+
+  // @todo
+  const formattedFile = generatedCodeWithImports;
+  // const formattedFile = await prettier.format(generatedCodeWithImports, {
+  //   filepath: ".ts",
+  // });
+
+  await fs.writeFile(
+    path.join(
+      import.meta.dirname,
+      `../__generated/${entClassName}Operators.ts`,
+    ),
+    formattedFile,
+  );
+
+  logger.info(
+    entSchemaCodegen.name,
+    `Generated '${entClassName}Operators' from '${entSchema.name}'`,
   );
 }
 
