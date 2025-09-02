ba196dca698e9f59d27fd999dfd9d493bc7c7620
Jaime <jaimeloeuf@gmail.com>
2025-09-03 00:21:27 +0800

    Add missing await for printing git status.

--- api/src/codegen/codegenTool/showGitStatusOfGeneratedFolder.ts
+++ api/src/codegen/codegenTool/showGitStatusOfGeneratedFolder.ts
@@ -3,8 +3,8 @@ import { generatedDocDirPathString } from "../codegenForDoc/index.js";
 import { generatedSrcDirPathString } from "../codegenForTs/index.js";
 
 export async function showGitStatusOfGeneratedFolder() {
-  printGitStatus(generatedSrcDirPathString);
+  await printGitStatus(generatedSrcDirPathString);
 
   // Use glob pattern to specify only generated docs
-  printGitStatus(`${generatedDocDirPathString}/**.generated.md`);
+  await printGitStatus(`${generatedDocDirPathString}/**.generated.md`);
 }
