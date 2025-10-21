c011e31938ad8a6b5f4841cd73a4e57704ed9355
Jaime <jaimeloeuf@gmail.com>
2025-10-21 23:38:08 +0800

    Export namespaced codegen libs.

--- api/src/codegen-lib/index.ts
+++ api/src/codegen-lib/index.ts
@@ -2,4 +2,6 @@
  * Barrel file.
  */
 
+export * as codegenForDoc from "./codegenForDoc/index.js";
+export * as codegenForTs from "./codegenForTs/index.js";
 export * from "./genAndSaveGeneratedFile.js";
