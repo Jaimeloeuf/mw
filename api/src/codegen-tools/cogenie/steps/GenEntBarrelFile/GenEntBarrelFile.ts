9a7ad1c30ff03366c28e0197d7c9e6aa41d87527
Jaime <jaimeloeuf@gmail.com>
2025-10-27 01:03:54 +0800

    Migrate cogenie step to use Class.

---
+++ api/src/codegen-tools/cogenie/steps/GenEntBarrelFile/GenEntBarrelFile.ts
@@ -0,0 +1,54 @@
+import type { CogenieStep } from "../../CogenieStep.js";
+
+import { codegenForTs } from "../../../../codegen-lib/index.js";
+import { getEntFolders } from "../../utils/index.js";
+
+/**
+ * Generate a single barrel file to re-export all the Ents in the ent/ folder
+ * under the `ent` namespace, so that other files that uses Ents outside of ent/
+ * can easily access them without having to import them manually/individually.
+ *
+ * This allows users to do something like
+ * `import { ent } from "../__generated/index.js";`
+ */
+export class GenEntBarrelFile implements CogenieStep {
+  getFiles() {
+    return {
+      entsExportFile: {
+        name: "entsExportFile",
+        extension: ".ts",
+      },
+      entsBarrelFile: {
+        name: "entsBarrelFile",
+        extension: ".ts",
+      },
+    } as const;
+  }
+
+  async generate() {
+    const entFolders = await getEntFolders();
+
+    const generatedCode = entFolders
+      .map(
+        (folder) => `export * from "../ents/${folder.name}/${folder.name}.js";
+`,
+      )
+      .join("");
+
+    await codegenForTs.genAndSaveGeneratedCode(
+      GenEntBarrelFile,
+      generatedCode,
+      this.getFiles().entsExportFile.name,
+
+      // Do not re-export this in the barrel file, as we want users to access
+      // all the Ents via the `ents` symbol as defined below.
+      { doNotIncludeInGeneratedFolderBarrelFile: true },
+    );
+
+    await codegenForTs.genAndSaveGeneratedCode(
+      GenEntBarrelFile,
+      `export * as ents from "./${this.getFiles().entsExportFile.name}${codegenForTs.generatedCodeFileExtensionWithNoBarrelFileInclusionForJsImport}"`,
+      this.getFiles().entsBarrelFile.name,
+    );
+  }
+}
