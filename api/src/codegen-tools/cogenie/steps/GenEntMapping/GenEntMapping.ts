fdb7bb48447369a2a5f66ccdedbdd118746deb3a
Jaime <jaimeloeuf@gmail.com>
2025-10-27 00:57:40 +0800

    Migrate cogenie step to use Class.

---
+++ api/src/codegen-tools/cogenie/steps/GenEntMapping/GenEntMapping.ts
@@ -0,0 +1,43 @@
+import type { CogenieStep } from "../../CogenieStep.js";
+
+import { codegenForTs } from "../../../../codegen-lib/index.js";
+import { getEntFolders } from "../../utils/index.js";
+
+/**
+ * Generate a mapping of `EntTypeID` to `Ent`
+ */
+export class GenEntMapping implements CogenieStep {
+  getFiles() {
+    return {
+      entMappingExportFile: {
+        name: "entMapping",
+        extension: ".ts",
+      },
+    } as const;
+  }
+
+  async generate() {
+    const ents = await getEntFolders();
+
+    const generatedCode = `import type { BaseEnt } from "../ent/BaseEnt.js";
+
+import { ents } from "./entsBarrelFile${codegenForTs.generatedCodeFileExtensionForJsImport}"
+
+/**
+ * Mapping of \`EntTypeID\` to \`Ent\`.
+ */
+export const entMapping: Record<
+  string,
+  new (..._constructorArgs: any) => BaseEnt<any>
+> = {
+  ${ents.map((ent) => `"${ent.entTypeID}": ents.${ent.name}`).join()}
+};
+`;
+
+    await codegenForTs.genAndSaveGeneratedCode(
+      GenEntMapping,
+      generatedCode,
+      this.getFiles().entMappingExportFile.name,
+    );
+  }
+}
