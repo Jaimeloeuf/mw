6a9207a7f6caab5eb6e54a724e6b61f44950ecc2
Jaime <jaimeloeuf@gmail.com>
2025-10-28 22:41:56 +0800

    Copy class based cogenie steps into steps2:     Temporarily using this folder for migration first to prevent git     case only updates issue.

---
+++ api/src/codegen-tools/cogenie/steps2/GenEntMapping/GenEntMapping.ts
@@ -0,0 +1,44 @@
+import type { CogenieStep } from "../../CogenieStep.js";
+
+import { codegenForTs } from "../../../../codegen-lib/index.js";
+import { getEntFolders } from "../../utils/index.js";
+import { GenEntBarrelFile } from "../GenEntBarrelFile/GenEntBarrelFile.js";
+
+/**
+ * Generate a mapping of `EntTypeID` to `Ent`
+ */
+export class GenEntMapping implements CogenieStep {
+  getFiles() {
+    return {
+      entMappingExportFile: {
+        name: "entMapping",
+        extension: ".ts",
+      },
+    } as const;
+  }
+
+  async generate() {
+    const ents = await getEntFolders();
+
+    const generatedCode = `import type { BaseEnt } from "../ent/BaseEnt.js";
+
+import { ents } from "./${new GenEntBarrelFile().getFiles().entsBarrelFile.name}${codegenForTs.generatedCodeFileExtensionForJsImport}"
+
+/**
+ * Mapping of \`EntTypeID\` to \`Ent\`.
+ */
+export const entMapping: Record<
+  string,
+  new (..._constructorArgs: any) => BaseEnt<any>
+> = {
+  ${ents.map((ent) => `"${ent.entTypeID}": ents.${ent.name}`).join()}
+};
+`;
+
+    await codegenForTs.genAndSaveGeneratedCode(
+      GenEntMapping,
+      generatedCode,
+      this.getFiles().entMappingExportFile.name,
+    );
+  }
+}
