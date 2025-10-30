97971b0e635b8fdee0b687b6e4593ebb5844e129
Jaime <jaimeloeuf@gmail.com>
2025-10-30 23:00:19 +0800

    Migrate cogenie step to use Class.

--- api/src/codegen-tools/cogenie/steps/genScriptsDoc/scriptTemplate.ts
+++
@@ -1,8 +0,0 @@
-export const scriptTemplate = (generatedConfigRows: string) => `# Scripts
-This documents all the available scripts [API server](../api/)
-
-
-## Scripts
-| Name | npm run command |
-| - | - |
-${generatedConfigRows}`;
---
+++ api/src/codegen-tools/cogenie/steps2/GenScriptsDoc/GenScriptsDoc.ts
@@ -0,0 +1,59 @@
+import type { CogenieStep } from "../../CogenieStep.js";
+
+import { codegenForDoc } from "../../../../codegen-lib/index.js";
+
+/**
+ * Generate documentation for all the available scripts for the API server.
+ */
+export class GenScriptsDoc implements CogenieStep {
+  getFiles() {
+    return {
+      scripts: {
+        name: "Scripts",
+        extension: ".md",
+      },
+    } as const;
+  }
+
+  async generate() {
+    const { default: packageJson } = await import(
+      "../../../../../package.json",
+      {
+        assert: {
+          type: "json",
+        },
+      }
+    );
+
+    const scriptCommands: Array<string> = [];
+    const scriptsPrefix = "scripts:";
+
+    for (const script in packageJson["scripts"]) {
+      if (script.startsWith(scriptsPrefix)) {
+        scriptCommands.push(script.slice(scriptsPrefix.length));
+      }
+    }
+
+    const scripts = scriptCommands
+      .map(
+        (scriptCommand) =>
+          `|${scriptCommand}|npm run ${scriptsPrefix}${scriptCommand}`,
+      )
+      .join("\n");
+
+    const generatedDoc = `# Scripts
+This documents all the available scripts [API server](../api/)
+
+
+## Scripts
+| Name | npm run command |
+| - | - |
+${scripts}`;
+
+    await codegenForDoc.genAndSaveGeneratedDoc(
+      GenScriptsDoc,
+      generatedDoc,
+      this.getFiles().scripts.name,
+    );
+  }
+}
