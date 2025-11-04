fb85e787dd4bb48e7e11ef9112556f5aa944bc0b
Jaime <jaimeloeuf@gmail.com>
2025-11-05 00:13:12 +0800

    Migrate cogenie step to use Class.

---
+++ api/src/codegen-tools/cogenie/steps2/GenHttpControllerBarrelFile/GenHttpControllerBarrelFile.ts
@@ -0,0 +1,55 @@
+import path from "path";
+
+import type { CogenieStep } from "../../CogenieStep.js";
+import type { HttpControllerFile } from "../../utils/index.js";
+
+import { codegenForTs } from "../../../../codegen-lib/index.js";
+import { getHttpControllerFiles } from "../../utils/index.js";
+
+/**
+ * Generate a single barrel file to re-export all the HTTP controllers in the
+ * controllers/ folder, so that other files that uses the controllers can easily
+ * access it without having to import them manually.
+ *
+ * This allows users to do something like `import * as c from "./httpControllerBarrelFile.generated.js";`
+ */
+export class GenHttpControllerBarrelFile implements CogenieStep {
+  getFiles() {
+    return {
+      httpControllerBarrelFile: {
+        name: "httpControllerBarrelFile",
+        extension: ".ts",
+      },
+    } as const;
+  }
+
+  async generate() {
+    const controllerFolderPath = path.join(
+      import.meta.dirname,
+      `../../../../controllers-http`,
+    );
+
+    const controllerFiles = await getHttpControllerFiles();
+
+    const generatedCode = controllerFiles
+      .map((file) => this.controllerExportTemplate(file, controllerFolderPath))
+      .join("");
+
+    await codegenForTs.genAndSaveGeneratedCode(
+      GenHttpControllerBarrelFile,
+      generatedCode,
+      this.getFiles().httpControllerBarrelFile.name,
+
+      // Do not re-export this in the barrel file, as this file will only be used
+      // by other generated files.
+      { doNotIncludeInGeneratedFolderBarrelFile: true },
+    );
+  }
+
+  controllerExportTemplate = (
+    file: HttpControllerFile,
+    controllerFolderPath: string,
+  ): string =>
+    `export { default as ${file.name} } from "../controllers-http/${path.relative(controllerFolderPath, file.path.replace(".ts", ".js"))}";
+`;
+}
