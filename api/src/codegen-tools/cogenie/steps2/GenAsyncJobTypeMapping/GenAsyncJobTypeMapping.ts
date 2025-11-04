362790755696911c3e24a7750016d070d1e1da0b
Jaime <jaimeloeuf@gmail.com>
2025-11-04 21:15:32 +0800

    Migrate cogenie step to use Class.

---
+++ api/src/codegen-tools/cogenie/steps2/GenAsyncJobTypeMapping/GenAsyncJobTypeMapping.ts
@@ -0,0 +1,54 @@
+import path from "path";
+
+import type { CogenieStep } from "../../CogenieStep.js";
+
+import { codegenForTs } from "../../../../codegen-lib/index.js";
+import { getAsyncJobTypeFiles } from "../../utils/index.js";
+
+/**
+ * Generate a mapping of `AsyncJobType['ID']` to `AsyncJobType`
+ */
+export class GenAsyncJobTypeMapping implements CogenieStep {
+  getFiles() {
+    return {
+      asyncJobTypeMapping: {
+        name: "asyncJobTypeMapping",
+        extension: ".ts",
+      },
+    } as const;
+  }
+
+  async generate() {
+    const folderPath = path.join(import.meta.dirname, `../../../../async/jobs`);
+
+    const files = await getAsyncJobTypeFiles();
+
+    const generatedCode = `import type { AsyncJobType } from "../async/AsyncJobType.js";
+
+/**
+ * Mapping of \`AsyncJobType['ID']\` to \`AsyncJobType\`.
+ *
+ * Use \`import\` function to asynchronously load the job definition module, so
+ * that they are not all loaded on startup and only lazily loaded whenever they
+ * are used.
+ */
+export const asyncJobTypeMapping: Record<
+  string,
+  () => Promise<{ default: AsyncJobType<any> }>
+> = {
+  ${files
+    .map(
+      (file) =>
+        `"${file.id}": () => import("../async/jobs/${path.relative(folderPath, file.path.replace(".ts", ".js"))}")`,
+    )
+    .join()}
+};
+`;
+
+    await codegenForTs.genAndSaveGeneratedCode(
+      GenAsyncJobTypeMapping,
+      generatedCode,
+      this.getFiles().asyncJobTypeMapping.name,
+    );
+  }
+}
