b83a64e9cd326af6ff0d46d8daea1d7f68995633
Jaime <jaimeloeuf@gmail.com>
2025-11-04 21:05:40 +0800

    Migrate cogenie step to use Class.

---
+++ api/src/codegen-tools/cogenie/steps2/GenServerConfigDoc/GenServerConfigDoc.ts
@@ -0,0 +1,58 @@
+import path from "path";
+
+import type { CogenieStep } from "../../CogenieStep.js";
+
+import { codegenForDoc } from "../../../../codegen-lib/index.js";
+import { logger } from "../../../../logging/index.js";
+import { getTypeofTypeAlias } from "./getTypeofTypeAlias.js";
+
+/**
+ * Generated documentation for all the available config values in API server.
+ */
+export class GenServerConfigDoc implements CogenieStep {
+  getFiles() {
+    return {
+      serverConfigs: {
+        name: "Server Configs",
+        extension: ".md",
+      },
+    } as const;
+  }
+
+  async generate() {
+    const configType = getTypeofTypeAlias(
+      path.resolve(import.meta.dirname, "../../../../config/ConfigType.ts"),
+      "ConfigType",
+    );
+
+    if (configType instanceof Error) {
+      logger.error(
+        `${GenServerConfigDoc.name}:${getTypeofTypeAlias.name}`,
+        configType,
+      );
+      return;
+    }
+
+    const generatedConfigRows = configType
+      .map(
+        ({ name, type }) =>
+          `${name}|${type.replaceAll("|", "\\|").replaceAll("<", "\\<").replaceAll(">", "\\>")}|`,
+      )
+      .join("\n");
+
+    const generatedCode = `# Server Config
+This documents all the available config values for [API server](../api/)
+
+
+## Configs
+| Config value | TS Type |
+| - | - |
+${generatedConfigRows}`;
+
+    await codegenForDoc.genAndSaveGeneratedDoc(
+      GenServerConfigDoc,
+      generatedCode,
+      this.getFiles().serverConfigs.name,
+    );
+  }
+}
