45bba5027e13a30f80377dda830c3459d1bb810d
Jaime <jaimeloeuf@gmail.com>
2025-11-06 22:44:08 +0800

    Migrate cogenie step to use Class.

---
+++ api/src/codegen-tools/cogenie/steps2/GenHttpRoutesTable/GenHttpRoutesTable.ts
@@ -0,0 +1,38 @@
+import type { CogenieStep } from "../../CogenieStep.js";
+
+import { codegenForTs } from "../../../../codegen-lib/index.js";
+import { getHttpControllerFiles } from "../../utils/index.js";
+import { routeDefinitionTemplate } from "./routeDefinitionTemplate.js";
+import { routeTableTemplate } from "./routeTableTemplate.js";
+
+/**
+ * Generate the HTTP API ExpressJS routes table file, by looking at all the HTTP
+ * controllers in the controllers/ folder.
+ */
+export class GenHttpRoutesTable implements CogenieStep {
+  getFiles() {
+    return {
+      registerRoutesAndControllers: {
+        name: "registerRoutesAndControllers",
+        extension: ".ts",
+      },
+    } as const;
+  }
+
+  async generate() {
+    const controllerFiles = await getHttpControllerFiles();
+
+    // Generate route definition statements, and combine them into a single string
+    const routeDefinitions = controllerFiles
+      .map((file) => routeDefinitionTemplate(file))
+      .join("");
+
+    const generatedCode = routeTableTemplate(routeDefinitions);
+
+    await codegenForTs.genAndSaveGeneratedCode(
+      GenHttpRoutesTable,
+      generatedCode,
+      this.getFiles().registerRoutesAndControllers.name,
+    );
+  }
+}
