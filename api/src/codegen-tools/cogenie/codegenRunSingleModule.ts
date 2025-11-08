b66c4b5e0608eaa4c8aed526da6ce76e61ae35fc
Jaime <jaimeloeuf@gmail.com>
2025-11-08 23:52:30 +0800

    Remove support for cogenie gen functions.

--- api/src/codegen-tools/cogenie/codegenRunSingleModule.ts
+++ api/src/codegen-tools/cogenie/codegenRunSingleModule.ts
@@ -3,9 +3,7 @@ import path from "path";
 
 import { codegenForTs } from "../../codegen-lib/index.js";
 import { logger } from "../../logging/index.js";
-import { loadCodegenFunction } from "./loadCodegenFunction.js";
 import { loadCogenieStep } from "./loadCogenieStep.js";
-import { runCodegenModules } from "./runCodegenModules.js";
 import { runCogenieSteps } from "./runCogenieSteps.js";
 
 export async function codegenRunSingleModule(codegenModuleName: string) {
@@ -31,40 +29,20 @@ export async function codegenRunSingleModule(codegenModuleName: string) {
     return;
   }
 
-  // @todo Remove conditional to keep only 1
-  if (codegenModuleName.startsWith("gen")) {
-    const importedCodegenFunction = await loadCodegenFunction(
-      codegenModuleName,
-      codegenModulePath,
-    );
+  const importedCogenieStep = await loadCogenieStep(
+    codegenModuleName,
+    codegenModulePath,
+  );
 
-    // For single module codegen, since we are unable to know if the output file
-    // path has changed, we must rely on users to manually delete it as we cannot
-    // delete all generated files since we do not regenerate all files.
-    logger.info(
-      codegenRunSingleModule.name,
-      "If your codegen module's output file has changed, please manually delete it first",
-    );
+  // For single module codegen, since we are unable to know if the output file
+  // path has changed, we must rely on users to manually delete it as we cannot
+  // delete all generated files since we do not regenerate all files.
+  logger.info(
+    codegenRunSingleModule.name,
+    "If your codegen module's output file has changed, please manually delete it first",
+  );
 
-    await runCodegenModules(importedCodegenFunction);
-  }
-
-  if (codegenModuleName.startsWith("Gen")) {
-    const importedCogenieStep = await loadCogenieStep(
-      codegenModuleName,
-      codegenModulePath,
-    );
-
-    // For single module codegen, since we are unable to know if the output file
-    // path has changed, we must rely on users to manually delete it as we cannot
-    // delete all generated files since we do not regenerate all files.
-    logger.info(
-      codegenRunSingleModule.name,
-      "If your codegen module's output file has changed, please manually delete it first",
-    );
-
-    await runCogenieSteps(importedCogenieStep);
-  }
+  await runCogenieSteps(importedCogenieStep);
 
   await codegenForTs.genCodegenBarrelFile();
 }
