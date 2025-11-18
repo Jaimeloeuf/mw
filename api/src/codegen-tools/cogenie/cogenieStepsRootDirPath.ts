7658b610796ee613c5038969eabb1d2a7c293378
Jaime <jaimeloeuf@gmail.com>
2025-11-18 19:54:53 +0800

    Update docs.

--- api/src/codegen-tools/cogenie/cogenieStepsRootDirPath.ts
+++ api/src/codegen-tools/cogenie/cogenieStepsRootDirPath.ts
@@ -1,7 +1,13 @@
 import path from "path";
 
-// Cogenie steps should be in the cogenie/steps folder, and should be in a
-// folder with the same name as itself
+/**
+ * Cogenie steps should be in the cogenie/steps folder, and should be defined in
+ * a folder with the same name as itself.
+ *
+ * ### Example
+ * A cogenie step called `GenSomething` should be defined in
+ * `cogenie/steps/GenSomething/GenSomething.ts`.
+ */
 export const cogenieStepsRootDirPath = path.resolve(
   import.meta.dirname,
   "./steps",
