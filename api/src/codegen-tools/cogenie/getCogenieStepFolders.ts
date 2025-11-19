90b37aa508fb4197ea4000993aa339262cb8b701
Jaime <jaimeloeuf@gmail.com>
2025-11-19 18:06:05 +0800

    Shift docs.

--- api/src/codegen-tools/cogenie/getCogenieStepFolders.ts
+++ api/src/codegen-tools/cogenie/getCogenieStepFolders.ts
@@ -4,6 +4,15 @@ import { cogenieStepsRootDirPath } from "./cogenieStepsRootDirPath.js";
 
 /**
  * Load all cogenie step folders and return them as a list of `Dirent` objects.
+ *
+ * Cogenie steps are located in folders under 'cogenie/steps' that start with
+ * the `Gen` prefix. For example, `GenHttpRoutesTable` starts with `Gen` and is
+ * therefore assumed to be a cogenie step folder.
+ *
+ * Cogenie steps need to have the same name as their folder name, and the
+ * main cogenie step class itself must also be the same name. For example,
+ * `GenHttpRoutesTable` is the name of the cogenie step folder, name of the
+ * module itself, and name of the exported cogenie step class.
  */
 export async function getCogenieStepFolders() {
   const dirents = await fs.readdir(cogenieStepsRootDirPath, {
--- api/src/codegen-tools/cogenie/loadAllCogenieSteps.ts
+++ api/src/codegen-tools/cogenie/loadAllCogenieSteps.ts
@@ -5,15 +5,6 @@ import { loadCogenieStep } from "./loadCogenieStep.js";
 
 /**
  * Dynamically load and return all cogenie steps.
- *
- * Cogenie steps are located in folders under 'cogenie/steps' that start with
- * the `Gen` prefix. For example, `GenHttpRoutesTable` starts with `Gen` and is
- * therefore assumed to be a cogenie step folder.
- *
- * Cogenie steps need to have the same name as their folder name, and the
- * main cogenie step class itself must also be the same name. For example,
- * `GenHttpRoutesTable` is the name of the cogenie step folder, name of the
- * module itself, and name of the exported cogenie step class.
  */
 export async function loadAllCogenieSteps() {
   const cogenieStepFolders = await getCogenieStepFolders();
