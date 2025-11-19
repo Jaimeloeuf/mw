58d0da94c98a2e0533d4419449b6b4911104e482
Jaime <jaimeloeuf@gmail.com>
2025-11-19 18:16:24 +0800

    Refactor out getGeneratedFilesDirent logic.

---
+++ api/src/codegen-tools/cogenie/getGeneratedFilesDirent.ts
@@ -0,0 +1,43 @@
+import fs from "fs/promises";
+
+import { codegenForTs, codegenForDoc } from "../../codegen-lib/index.js";
+
+export async function getGeneratedFilesDirent() {
+  const generatedCodeFilesDirent = await fs
+    .readdir(codegenForTs.generatedSrcDirPath, {
+      recursive: true,
+      withFileTypes: true,
+    })
+    .then((dirent) =>
+      // Filter for generated files
+      dirent.filter(
+        (file) =>
+          file.isFile() &&
+          (file.name.endsWith(codegenForTs.generatedCodeFileExtension) ||
+            file.name.endsWith(
+              codegenForTs.generatedCodeFileExtensionWithNoBarrelFileInclusion,
+            )),
+      ),
+    );
+
+  const generatedDocFilesDirent = await fs
+    .readdir(codegenForDoc.generatedDocDirPath, {
+      recursive: true,
+      withFileTypes: true,
+    })
+    .then((dirent) =>
+      // Filter for generated files
+      dirent.filter(
+        (file) =>
+          file.isFile() &&
+          file.name.endsWith(codegenForDoc.generatedDocFileExtension),
+      ),
+    );
+
+  const generatedFilesDirent = [
+    ...generatedCodeFilesDirent,
+    ...generatedDocFilesDirent,
+  ];
+
+  return generatedFilesDirent;
+}
