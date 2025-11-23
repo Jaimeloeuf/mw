2e69d3d790170e8c1d968204feee5cb0808bd6fc
Jaime <jaimeloeuf@gmail.com>
2025-11-23 14:50:31 +0800

    Remove index.ts cogenie generation:     This should be left untouched from now on.

--- api/src/codegen-lib/codegenForTs/genCodegenBarrelFile.ts
+++ api/src/codegen-lib/codegenForTs/genCodegenBarrelFile.ts
@@ -1,9 +1,7 @@
 import fs from "fs/promises";
 
-import { genAndSaveGeneratedFile } from "../genAndSaveGeneratedFile.js";
 import { genAndSaveGeneratedCode } from "./genAndSaveGeneratedCode.js";
 import { generatedSrcDirPath } from "./generatedSrcDirPath.js";
-import { genGeneratedNotice } from "./genGeneratedNotice.js";
 
 /**
  * Generate barrel file for all generated files
@@ -27,18 +25,4 @@ export async function genCodegenBarrelFile() {
     "\n";
 
   await genAndSaveGeneratedCode(genCodegenBarrelFile, generatedCode, "index");
-  await genAndSaveGeneratedFile({
-    generator: genCodegenBarrelFile,
-    genGeneratedNotice,
-    generatedText: `// This index.ts should NEVER CHANGE
-// Because this index.ts is a special case file without the ".generated" file
-// extension it does not work with cogenie handling well so this is only
-// generated once and left alone, and the actual barrel file is in the
-// ".generated" version of this index.ts file.
-export * from "./index.generated.js";`,
-    generatedFileRootDirPath: generatedSrcDirPath,
-    generatedTextFileName: "index",
-    generatedTextFileType: ".ts",
-    generatedTextFileNameExtension: ".ts",
-  });
 }
