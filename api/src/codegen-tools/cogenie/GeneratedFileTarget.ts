774319c8f42fa9e22e919bcda01246b2674a4b8b
Jaime <jaimeloeuf@gmail.com>
2025-11-24 20:16:33 +0800

    Add in support for using generated file extensions:     In the GeneratedFileTarget type, so that everything about the     generated file target is defined in this type rather than partly     here and partly in the call to genAndSaveGenerated... util functions.

--- api/src/codegen-tools/cogenie/GeneratedFileTarget.ts
+++ api/src/codegen-tools/cogenie/GeneratedFileTarget.ts
@@ -1,7 +1,15 @@
+import type { codegenForTs, codegenForDoc } from "../../codegen-lib/index.js";
+
 /**
  * Details of the generated file
  */
 export type GeneratedFileTarget = {
   name: string;
-  extension: ".ts" | ".md";
+  // @todo Delete raw extensions
+  extension:
+    | ".ts"
+    | ".md"
+    | typeof codegenForTs.generatedCodeFileExtension
+    | typeof codegenForTs.generatedCodeFileExtensionWithNoBarrelFileInclusion
+    | typeof codegenForDoc.generatedDocFileExtension;
 };
