c74953a5bffc3a1a72189673e8388b8a608592f3
Jaime <jaimeloeuf@gmail.com>
2026-04-07 23:32:43 +0800

    Change EntEdgeDefinition type definitions:     Change from type alias to interface so that the override type is     clear and structured.

--- api/src/entschema/lib/EntEdgeDefinition.ts
+++ api/src/entschema/lib/EntEdgeDefinition.ts
@@ -1,13 +1,20 @@
-export type EntFieldEdgeDefinition = {
-  type: "field-edge";
-  idField: string;
-};
-
-export type EntAssocEdgeDefinition = {
-  type: "assoc-edge";
-};
-
-export type EntEdgeDefinition = {
+export type BaseEntEdgeDefinition = {
+  /**
+   * The specific Ent Edge type definition should override this to a string
+   * literal.
+   */
+  type: string;
   name: string;
   description?: string;
-} & (EntFieldEdgeDefinition | EntAssocEdgeDefinition);
+};
+
+export interface EntFieldEdgeDefinition extends BaseEntEdgeDefinition {
+  type: "field-edge";
+  idField: string;
+}
+
+export interface EntAssocEdgeDefinition extends BaseEntEdgeDefinition {
+  type: "assoc-edge";
+}
+
+export type EntEdgeDefinition = EntFieldEdgeDefinition | EntAssocEdgeDefinition;
