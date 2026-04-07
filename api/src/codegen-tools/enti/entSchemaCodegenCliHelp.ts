d88402510248cc91e3dad15caac0cc1d1c129b36
Jaime <jaimeloeuf@gmail.com>
2026-04-08 00:10:24 +0800

    Fix enti CLI menu.

--- api/src/codegen-tools/enti/entSchemaCodegenCliHelp.ts
+++ api/src/codegen-tools/enti/entSchemaCodegenCliHelp.ts
@@ -3,22 +3,20 @@ import pc from "picocolors";
 export function entSchemaCodegenCliHelp() {
   /* eslint-disable no-console */
   console.log("See help menu");
-  console.log(pc.green("npm run scripts:enti"));
-  console.log(pc.green("npm run scripts:enti help"));
+  console.log(pc.green("npm run codegen:enti"));
+  console.log(pc.green("npm run codegen:enti help"));
   console.log();
-  console.log("See all available scripts:enti modules");
-  console.log(pc.green("npm run scripts:enti list"));
+  console.log("See all available EntSchemas");
+  console.log(pc.green("npm run codegen:enti list"));
   console.log();
-  console.log("Validate that generated code is not manually modified");
-  console.log(pc.green("npm run scripts:enti validate"));
+  console.log("Validate EntSchemas");
+  console.log(pc.green("npm run codegen:enti validate"));
   console.log();
-  console.log("Run all scripts:enti modules");
-  console.log(pc.green("npm run scripts:enti all"));
+  console.log("Generate all EntSchema artifacts");
+  console.log(pc.green("npm run codegen:enti all"));
   console.log();
-  console.log(
-    "Run a single scripts:enti module, e.g. run the module genHttpRoutesTable",
-  );
-  console.log(pc.green("npm run scripts:enti genHttpRoutesTable"));
+  console.log("Generate a single EntSchema artifact");
+  console.log(pc.green("npm run codegen:enti EntJohariSchema"));
   console.log();
   console.log(`For more details, see implementation: ${import.meta.dirname}`);
   /* eslint-enable no-console */
